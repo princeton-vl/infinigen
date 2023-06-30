338e9bc184be02b1af034cd1c8a1711a34acd988
Zeyu Ma <zeyum@princeton.edu>
2023-06-30 03:11:30 -0400

    Add 34 lines last edited by Zeyu Ma in worldgen/tools/dev/landtile_viewer.py
    Commit made automatically to show authorship. This version of the code is not usable.

---
+++ worldgen/tools/dev/landtile_viewer.py
@@ -0,0 +1,34 @@
+import os
+import sys
+
+sys.path.append(f"{os.path.split(os.path.abspath(__file__))[0]}/../..")
+import argparse
+
+import bpy
+import numpy as np
+from nodes.node_wrangler import Nodes, NodeWrangler
+from terrain.utils import Mesh, read
+from util.blender import clear_scene
+from util.organization import AssetFile
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', type=str)
+    parser.add_argument('-o', '--overlay', type=int, default=False)
+    args = parser.parse_args(sys.argv[sys.argv.index("--") + 1:])
+    folder = os.path.dirname(args.input)
+    tile_size = float(np.loadtxt(f"{folder}/{AssetFile.TileSize}.txt"))
+    image = read(args.input)
+    mesh = Mesh(heightmap=image, L=tile_size)
+    if args.overlay:
+        image = read(args.input.replace(AssetFile.Heightmap, AssetFile.Mask))
+        mesh.vertex_attributes["attribute"] = image.reshape(-1).astype(np.float32)
+    clear_scene()
+    obj = mesh.export_blender("preview")
+    if args.overlay:
+        material = bpy.data.materials.new(name="preview_material")
+        material.use_nodes = True
+        nw = NodeWrangler(material.node_tree)
+        new_attribute_node = nw.new_node(Nodes.Attribute, [], {"attribute_name": "attribute"})
+        material.node_tree.links.new(new_attribute_node.outputs['Color'], material.node_tree.nodes['Principled BSDF'].inputs['Base Color'])
+        obj.active_material = material
