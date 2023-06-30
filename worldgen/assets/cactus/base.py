00b915be7c9bd164e32fdc650307ff5d29512d5b
Lingjie Mei <lm5483@princeton.edu>
2023-06-30 03:11:23 -0400

    Add 21 lines last edited by Lingjie Mei in worldgen/assets/cactus/base.py
    Commit made automatically to show authorship. This version of the code is not usable.

---
+++ worldgen/assets/cactus/base.py
@@ -0,0 +1,21 @@
+import bpy
+
+from assets.cactus.spike import make_default_selections
+from assets.utils.decorate import write_attribute
+from nodes.node_info import Nodes
+from nodes.node_wrangler import NodeWrangler
+from placement.factory import AssetFactory
+
+class BaseCactusFactory(AssetFactory):
+    spike_distance = .025
+    cap_percentage = .1
+    noise_strength = .02
+    base_radius = .002
+    density = 5e4
+
+    def __init__(self, factory_seed, coarse=False):
+        super(BaseCactusFactory, self).__init__(factory_seed, coarse)
+        self.points_fn = make_default_selections(self.spike_distance, self.cap_percentage, self.density)
+
+    def create_asset(self, **params) -> bpy.types.Object:
+        raise NotImplemented
