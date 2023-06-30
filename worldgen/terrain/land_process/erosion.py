1683a30bec220ecaffe33d2f4570100800d29d36
Zeyu Ma <zeyum@princeton.edu>
2023-06-30 03:11:45 -0400

    Add 66 lines last edited by Zeyu Ma in worldgen/terrain/land_process/erosion.py
    Commit made automatically to show authorship. This version of the code is not usable.

---
+++ worldgen/terrain/land_process/erosion.py
@@ -0,0 +1,66 @@
+from ctypes import POINTER, c_char_p, c_float, c_int32
+
+import cv2
+import gin
+import numpy as np
+from numpy import ascontiguousarray as AC
+from terrain.utils import ASFLOAT, load_cdll, read, smooth
+from util.organization import AssetFile, Process
+
+
+@gin.configurable
+def run_erosion(
+    folder,
+    Ns=[512, 2048],
+    n_iters = [int(1e4), int(5e5)],
+    mask_height_range=None,
+    spatial=1,
+    mask_range=(4, 47),
+    ground_depth=25,
+    sinking_rate=0.05,
+):
+    dll = load_cdll(f"terrain/lib/cpu/soil_machine/SoilMachine.so")
+    func = dll.run
+    func.argtypes = [
+        POINTER(c_float), POINTER(c_float), POINTER(c_float),
+        c_int32, c_int32, c_int32, c_int32, c_int32, c_float, c_char_p
+    ]
+    func.restype = None
+
+    heightmap = read(str(folder/f'{AssetFile.Heightmap}.exr')).astype(np.float32)
+    tile_size = float(np.loadtxt(f"{folder}/{AssetFile.TileSize}.txt"))
+
+    for i, N, n_iter in zip(list(range(len(Ns))), Ns, n_iters):
+        M = heightmap.shape[0]
+        heightmap = cv2.resize(heightmap, (N, N))
+        if N > M: heightmap = smooth(heightmap, 3)
+        original_heightmap = heightmap.copy()
+        ground_level = heightmap.min() - ground_depth
+        heightmap = AC((heightmap - ground_level).astype(np.float32))
+        result_heightmap = np.zeros_like(heightmap)
+        watertrack = np.zeros_like(heightmap)
+        func(
+            ASFLOAT(heightmap),  ASFLOAT(result_heightmap),  ASFLOAT(watertrack),
+            N, N, 0, n_iter, 0, spatial * tile_size, "terrain/source/cpu/soil_machine/soil/sand.soil".encode('utf-8'),
+        )
+        heightmap = result_heightmap + ground_level
+        watertrack = watertrack.reshape((N, N))
+        watertrack = np.clip((watertrack - mask_range[0]) / (mask_range[1] - mask_range[0]), a_min=0, a_max=1)
+        watertrack = watertrack ** 0.2
+        if mask_height_range is not None:
+            mask = np.clip((heightmap - mask_height_range[0]) / (mask_height_range[1] - mask_height_range[0]), a_min=0, a_max=1)
+        else:
+            mask = np.ones_like(heightmap)
+        if i == 0 and len(Ns) > 1: heightmap -= watertrack * sinking_rate
+        heightmap = heightmap * mask + original_heightmap * (1 - mask)
+    if mask_height_range is not None:
+        kernel = np.ones((5, 5), np.float32) / 25
+        original_heightmap = heightmap.copy()
+        for i in range(5):
+            heightmap = cv2.filter2D(heightmap, -1, kernel)
+        heightmap = heightmap * (1 - mask) + original_heightmap * mask
+
+    cv2.imwrite(str(folder/f'{Process.Erosion}.{AssetFile.Heightmap}.exr'), heightmap)
+    cv2.imwrite(str(folder/f'{Process.Erosion}.{AssetFile.Mask}.exr'), watertrack)
+
+    del dll
