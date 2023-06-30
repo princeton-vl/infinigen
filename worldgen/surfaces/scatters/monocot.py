c65b9a2b73386b1243b4f35bb614ca46d01b69d7
Alexander Raistrick <araistrick@princeton.edu>
2023-06-30 03:11:08 -0400

    Add 16 lines last edited by Alexander Raistrick in worldgen/surfaces/scatters/monocot.py
    Commit made automatically to show authorship. This version of the code is not usable.

---
+++ worldgen/surfaces/scatters/monocot.py
@@ -0,0 +1,16 @@
+from numpy.random import uniform as U
+from placement.factory import AssetFactory, make_asset_collection
+from surfaces.scatters.utils.wind import wind
+def apply(obj, n=4, grass=None, selection=None, **kwargs):
+ 
+    monocots = make_asset_collection(
+        MonocotFactory(np.random.randint(1e5), grass=grass),  
+        n=n, verbose=True, **kwargs)
+    scatter_obj = scatter_instances(
+        base_obj=obj, collection=monocots,
+        vol_density=U(0.2, 4), min_spacing=0.1, 
+        ground_offset=(0, 0, -0.05),
+        scale=U(0.05, 0.4), scale_rand=U(0.5, 0.95), 
+        rotation_offset=wind(strength=20),
+        normal_fac=0.3,
+        selection=selection)
