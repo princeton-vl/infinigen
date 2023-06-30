65b51de615a5f001e70d1e68820eba0fa15ac571
Alexander Raistrick <araistrick@princeton.edu>
2023-06-30 03:11:11 -0400

    Add 56 lines last edited by Alexander Raistrick in worldgen/assets/glowing_rocks.py
    Commit made automatically to show authorship. This version of the code is not usable.

---
+++ worldgen/assets/glowing_rocks.py
@@ -0,0 +1,56 @@
+import bpy
+import gin
+import numpy as np
+from util import blender as butil
+
+from nodes.node_wrangler import Nodes, NodeWrangler
+from placement.factory import AssetFactory, make_asset_collection
+from surfaces.scatters.rocks import BlenderRockFactory
+from surfaces import surface
+from nodes.color import color_category
+
+    object_info = nw.new_node(Nodes.ObjectInfo_Shader)
+                                input_kwargs={"Vector": (object_info, "Random")})
+    mix_rgb = nw.new_node(Nodes.MixRGB, [0.6, (white_noise, "Color"), tuple(color_category("gem"))])
+    translucent_bsdf = nw.new_node(Nodes.TranslucentBSDF, [mix_rgb])
+    transparent_bsdf = nw.new_node(Nodes.TransparentBSDF, [mix_rgb])
+    is_camera_ray = nw.new_node(Nodes.LightPath) if transparent_for_bounce else 1
+    mix_shader = nw.new_node(Nodes.MixShader, [is_camera_ray, transparent_bsdf, translucent_bsdf])
+    nw.new_node(Nodes.MaterialOutput, [mix_shader])
+
+@gin.configurable
+class GlowingRocksFactory(AssetFactory):
+
+    def __init__(self, factory_seed, coarse=False, transparent_for_bounce=True, watt_power_range=(500, 1200), **kwargs):
+        super().__init__(factory_seed, coarse=coarse)
+        if coarse:
+            return
+        self.watt_power_range = watt_power_range
+        
+        for o in self.rock_collection.objects:
+            butil.modify_mesh(o, 'SUBSURF', levels=2)
+
+        self.material = surface.shaderfunc_to_material(shader_glowrock)
+
+    def create_placeholder(self, i, loc, rot):
+        placeholder = butil.spawn_empty('placeholder', disp_type='SPHERE', s=0.1)
+        return placeholder
+        
+    def create_asset(self, *args, **kwargs) -> bpy.types.Object:
+        src_obj = np.random.choice(list(self.rock_collection.objects))
+
+        new_obj = src_obj.copy()
+        new_obj.data = src_obj.data
+        new_obj.animation_data_clear()
+        bpy.context.collection.objects.link(new_obj)
+
+        new_obj.rotation_euler = np.random.uniform(-np.pi, np.pi, 3)
+        new_obj.scale = np.random.uniform(0.7, 1.5, 3) * 0.5
+        new_obj.active_material = self.material
+        min_side_length = (bbox.max(axis=0) - bbox.min(axis=0)).min()
+
+        # Diameter is set to half the shortest edge of the bbox
+        point_light = bpy.context.selected_objects[0]
+        point_light.data.energy = round(np.random.uniform(*self.watt_power_range))
+        point_light.parent = new_obj
+        return new_obj
