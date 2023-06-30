be1af2f979eabbcf8b2155b4da1f2bf06bf32303
Lingjie Mei <lm5483@princeton.edu>
2023-06-30 03:11:20 -0400

    Add 5 lines last edited by Lingjie Mei in worldgen/assets/deformed_trees/__init__.py
    Commit made automatically to show authorship. This version of the code is not usable.

---
+++ worldgen/assets/deformed_trees/__init__.py
@@ -0,0 +1,5 @@
+from .fallen import FallenTreeFactory
+from .rotten import RottenTreeFactory
+from .truncated import TruncatedTreeFactory
+from .hollow import HollowTreeFactory
+from .generate import DeformedTreeFactory
