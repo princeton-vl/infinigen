266c8a7c7b7135b7743d044046d99d7ecbbc8616
Lingjie Mei <lm5483@princeton.edu>
2023-06-30 03:11:19 -0400

    Add 33 lines last edited by Lingjie Mei in worldgen/assets/creatures/parts/crustacean/eye.py
    Commit made automatically to show authorship. This version of the code is not usable.

---
+++ worldgen/assets/creatures/parts/crustacean/eye.py
@@ -0,0 +1,33 @@
+import bpy
+import numpy as np
+from numpy.random import uniform
+
+from assets.creatures.creature import Part, PartFactory
+from assets.utils.decorate import join_objects
+from assets.utils.object import new_icosphere, origin2leftmost
+from placement.detail import remesh_with_attrs
+
+
+class CrustaceanEyeFactory(PartFactory):
+    tags = ['eye']
+    min_spike_distance = .05
+    min_spike_radius = .02
+
+    def make_part(self, params) -> Part:
+        length = params['length']
+        sphere = new_icosphere(radius=params['radius'])
+        bpy.ops.mesh.primitive_cylinder_add(radius=.01, depth=length, location=(-length / 2, 0, 0))
+        cylinder = bpy.context.active_object
+        cylinder.rotation_euler[1] = np.pi / 2
+        obj = join_objects([sphere, cylinder])
+        remesh_with_attrs(obj, .005)
+        origin2leftmost(obj)
+
+        skeleton = np.zeros((2, 3))
+        skeleton[1, 0] = length
+        return Part(skeleton, obj)
+
+    def sample_params(self):
+        radius = uniform(.03, .05)
+        length = radius * uniform(1, 1.5)
+        return {'radius': radius, 'length': length}
