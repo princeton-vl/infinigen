557ec14f320f92022152909b8f135fb94ad0b827
Lingjie Mei <lm5483@princeton.edu>
2023-06-30 03:11:26 -0400

    Add 46 lines last edited by Lingjie Mei in worldgen/assets/monocot/generate.py
    Commit made automatically to show authorship. This version of the code is not usable.

---
+++ worldgen/assets/monocot/generate.py
@@ -0,0 +1,46 @@
+import bpy
+import numpy as np
+from numpy.random import uniform
+
+from .veratrum import VeratrumMonocotFactory
+from .banana import BananaMonocotFactory, TaroMonocotFactory
+from .agave import AgaveMonocotFactory
+from .grasses import GrassesMonocotFactory, MaizeMonocotFactory, WheatMonocotFactory
+from .growth import MonocotGrowthFactory
+from .tussock import TussockMonocotFactory
+from placement.factory import AssetFactory
+from util.math import FixedSeed
+from ..utils.decorate import join_objects
+from ..utils.mesh import polygon_angles
+
+class MonocotFactory(AssetFactory):
+    max_cluster = 10
+
+    def create_asset(self, i, **params) -> bpy.types.Object:
+        if self.factory.is_grass:
+            n = np.random.randint(1, 6)
+            angles = polygon_angles(n, np.pi / 4, np.pi * 2)
+            radius = uniform(.08, .16, n)
+            monocots = [self.factory.create_asset(**params, i=j + i * self.max_cluster) for j in range(n)]
+            for m, a, r in zip(monocots, angles, radius):
+                m.location = r * np.cos(a), r * np.sin(a), 0
+        else:
+            m = self.factory.create_asset(**params)
+            return m
+
+    def __init__(self, factory_seed, coarse=False, factory_method=None, grass=None):
+        super(MonocotFactory, self).__init__(factory_seed, coarse)
+        with FixedSeed(factory_seed):
+            grass_factory = [TussockMonocotFactory, GrassesMonocotFactory, WheatMonocotFactory,
+                MaizeMonocotFactory]
+            nongrass_factory = [AgaveMonocotFactory, BananaMonocotFactory, TaroMonocotFactory,
+                VeratrumMonocotFactory]
+            # noinspection PyTypeChecker
+            self.factory_methods = grass_factory + nongrass_factory if grass is None else grass_factory if \
+                grass else nongrass_factory
+            weights = np.array([1] * len(self.factory_methods))
+            self.weights = weights / weights.sum()
+            if factory_method is None:
+                with FixedSeed(self.factory_seed):
+                    factory_method = np.random.choice(self.factory_methods, p=self.weights)
+            self.factory: MonocotGrowthFactory = factory_method(factory_seed, coarse)
