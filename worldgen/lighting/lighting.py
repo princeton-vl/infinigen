ef37342e4767861f62080077b983882479786788
Alexander Raistrick <araistrick@princeton.edu>
2023-06-30 03:11:10 -0400

    Add 23 lines last edited by Alexander Raistrick in worldgen/lighting/lighting.py
    Commit made automatically to show authorship. This version of the code is not usable.

--- worldgen/lighting/lighting.py
+++ worldgen/lighting/lighting.py
@@ -1,5 +1,10 @@
+import bpy
+import numpy as np
 import gin
 from util.random import random_general as rg
+
+from nodes.node_wrangler import Nodes, NodeWrangler
+from util.math import clip_gaussian
 @gin.configurable
 def nishita_lighting(
     nw,
@@ -11,6 +16,8 @@ def nishita_lighting(
     rising_angle=90,
     camera_based_rotation=None,
 ):
+    sky_texture = nw.new_node(Nodes.SkyTexture)
+    sky_texture.sun_size = np.deg2rad(clip_gaussian(0.5, 0.3, 0.25, 5))
     sky_texture.sun_intensity = rg(sun_intensity)
     sky_texture.sun_elevation = np.radians(rg(sun_elevation))
     if camera_based_rotation is None:
@@ -29,10 +36,26 @@ def nishita_lighting(
         sky_texture.sun_elevation = -np.radians(5)
         sky_texture.keyframe_insert(data_path="sun_elevation", frame=bpy.context.scene.frame_start + 10)
 
+    sky_texture.altitude = clip_gaussian(100, 400, 0, 2000)
     sky_texture.air_density =rg(air_density)
     sky_texture.dust_density = rg(dust_density)
+    sky_texture.ozone_density = clip_gaussian(1, 1, 0.1, 10)
+
 def add_lighting(cam=None):
+    nw = NodeWrangler(bpy.context.scene.world.node_tree)
+
+    if True:
         surface = nishita_lighting(nw, cam)
+    else:
+        # TODO more options
+        surface = None
+
+    volume = None
+
+    nw.new_node(Nodes.WorldOutput, input_kwargs={
+        'Surface': surface,
+        'Volume': volume
+    })
 @gin.configurable
     spot.data.energy = rg(energy)
     spot.data.spot_size = rg(spot_size)
