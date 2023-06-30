97b66d125596d6272f6225823d8c06b6a65c301a
Alexander Raistrick <araistrick@princeton.edu>
2023-06-30 03:11:13 -0400

    Add 4 lines last edited by Alexander Raistrick in worldgen/assets/creatures/__init__.py
    Commit made automatically to show authorship. This version of the code is not usable.

---
+++ worldgen/assets/creatures/__init__.py
@@ -0,0 +1,4 @@
+from .genomes.beetle import BeetleFactory, AntSwarmFactory
+from .genomes.carnivore import CarnivoreFactory
+from .genomes.fish import FishFactory, FishSchoolFactory
+from .genomes.reptile import FrogFactory, LizardFactory, SnakeFactory
