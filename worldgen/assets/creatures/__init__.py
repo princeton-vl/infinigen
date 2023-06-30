6c72d20085c5ab94acd05c0fec6f2918d5bcd105
Lingjie Mei <lm5483@princeton.edu>
2023-06-30 03:11:13 -0400

    Add 2 lines last edited by Lingjie Mei in worldgen/assets/creatures/__init__.py
    Commit made automatically to show authorship. This version of the code is not usable.

--- worldgen/assets/creatures/__init__.py
+++ worldgen/assets/creatures/__init__.py
@@ -1,4 +1,6 @@
 from .genomes.beetle import BeetleFactory, AntSwarmFactory
 from .genomes.carnivore import CarnivoreFactory
 from .genomes.fish import FishFactory, FishSchoolFactory
+from .genomes.herbivore import HerbivoreFactory
+from .genomes.crustacean import CrustaceanFactory, CrabFactory, LobsterFactory, SpinyLobsterFactory
 from .genomes.reptile import FrogFactory, LizardFactory, SnakeFactory
