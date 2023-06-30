7410f065a94ef333f3fab33604c0cc093777338e
Lingjie Mei <lm5483@princeton.edu>
2023-06-30 03:11:22 -0400

    Add 98 lines last edited by Lingjie Mei in worldgen/assets/cactus/spike.py
    Commit made automatically to show authorship. This version of the code is not usable.

---
+++ worldgen/assets/cactus/spike.py
@@ -0,0 +1,98 @@
+import colorsys
+import numpy as np
+from numpy.random import uniform
+
+from util import blender as butil
+from assets.utils.misc import sample_direction
+from assets.utils.decorate import assign_material
+from assets.utils.nodegroup import geo_radius
+from nodes.node_wrangler import NodeWrangler, Nodes
+from surfaces import surface
+from assets.trees.tree import build_radius_tree
+import util.blender as butil
+
+def build_spikes(base_radius=.002, **kwargs):
+    n_branch = 4
+    n_major = 9
+    branch_config = {
+        'n': n_branch,
+        'path_kargs': lambda idx: {'n_pts': n_major, 'std': .5, 'momentum': .85, 'sz': uniform(.005, .01)},
+        'spawn_kargs': lambda idx: {'init_vec': sample_direction(.8)}
+    }
+
+    radius_fn = lambda base_radius, size, resolution: base_radius * .5 ** (
+            np.arange(size * resolution) / (size * resolution))
+    obj = build_radius_tree(radius_fn, branch_config, base_radius)
+    surface.add_geomod(obj, geo_radius, apply=True, input_args=['radius', None, .001])
+    return obj
+
+
+def make_default_selections(spike_distance, cap_percentage, density):
+    def selection(nw: NodeWrangler, selected, geometry):
+        z = nw.separate(nw.new_node(Nodes.InputPosition))[-1]
+        z_stat = nw.new_node(Nodes.AttributeStatistic, [geometry, None, z]).outputs
+        percentage = nw.scalar_divide(nw.scalar_sub(z_stat['Max'], z), z_stat['Range'])
+        is_cap = nw.bernoulli(nw.build_float_curve(percentage, [(0, 1), (cap_percentage, .5), (1, 0)]))
+        cap = nw.new_node(Nodes.SeparateGeometry, [geometry, is_cap])
+        cap = nw.new_node(Nodes.MergeByDistance, [cap, None, spike_distance / 2])
+
+        points = nw.new_node(Nodes.DistributePointsOnFaces,
+                             input_kwargs={'Mesh': geometry, 'Selection': selected, 'Density': density
+                             }).outputs['Points']
+        points = nw.new_node(Nodes.MergeByDistance, [points, None, spike_distance])
+
+        all_points = nw.new_node(Nodes.JoinGeometry, [[cap, points]])
+        return all_points
+
+    return selection
+
+
+def geo_spikes(nw: NodeWrangler, spikes, points_fn=None, realize=True):
+    geometry, selection = nw.new_node(Nodes.GroupInput, expose_input=[('NodeSocketGeometry', 'Geometry', None),
+        ('NodeSocketFloat', 'Selection', None)]).outputs[:2]
+    capture = nw.new_node(Nodes.CaptureAttribute,
+                          input_kwargs={'Geometry': geometry, 'Value': nw.new_node(Nodes.InputNormal)})
+
+    selected = nw.compare('GREATER_THAN', selection, .8)
+    spikes = nw.new_node(Nodes.CollectionInfo, [spikes, True, True])
+
+    rotation = nw.new_node(Nodes.AlignEulerToVector, input_kwargs={'Vector': (capture, 'Attribute')},
+                           attrs={'axis': 'Z'})
+    rotation = nw.new_node(Nodes.RotateEuler,
+                           input_kwargs={'Rotation': rotation, 'Angle': nw.uniform(0, 2 * np.pi)},
+                           attrs={'type': 'AXIS_ANGLE', 'space': 'LOCAL'})
+    rotation = nw.new_node(Nodes.AlignEulerToVector, [rotation, nw.uniform(.2, .5)], attrs={'axis': 'Z'})
+    rotation = nw.add(rotation, nw.uniform([-.05] * 3, [.05] * 3))
+
+    spikes = nw.new_node(Nodes.InstanceOnPoints, input_kwargs={
+        'Points': points,
+        'Instance': spikes,
+        'Pick Instance': True,
+        'Rotation': rotation,
+        'Scale': nw.uniform([.5] * 3, [1.] * 3)
+    })
+
+
+def shader_spikes(nw: NodeWrangler):
+    roughness = .8
+    specular = .25
+    mix_ratio = .9
+    color = *colorsys.hsv_to_rgb(uniform(.2, .4), uniform(.1, .3), .8), 1
+    principled_bsdf = nw.new_node(Nodes.PrincipledBSDF, input_kwargs={
+        'Base Color': color,
+        'Roughness': roughness,
+        'Specular': specular,
+        'Subsurface': .1
+    })
+    transparent_bsdf = nw.new_node(Nodes.TranslucentBSDF, [color])
+    mix_rgb = nw.new_node(Nodes.MixShader, [mix_ratio, principled_bsdf, transparent_bsdf])
+    return mix_rgb
+
+
+def apply(obj, points_fn, base_radius=.002, realize=True):
+    spikes = deep_clone_obj(obj)
+    instances = make_asset_collection(build_spikes, 5, 'spikes', verbose=False, base_radius=base_radius)
+    surface.add_geomod(spikes, geo_spikes, apply=realize, input_args=[instances, points_fn, realize],
+                       input_attributes=[None, 'selection'])
+    butil.delete_collection(instances)
+    return spikes
