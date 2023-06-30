7ac218a43b66d065bb018603dbec76751057f796
Zeyu Ma <zeyum@princeton.edu>
2023-06-30 03:11:08 -0400

    Add 10 lines last edited by Zeyu Ma in worldgen/surfaces/scatters/snow_layer.py
    Commit made automatically to show authorship. This version of the code is not usable.

--- worldgen/surfaces/scatters/snow_layer.py
+++ worldgen/surfaces/scatters/snow_layer.py
@@ -10,4 +10,14 @@ from nodes import node_utils
 
 from surfaces.templates import snow
 
+class Snowlayer:
+    def __init__(self):
+        pass
     
+    def apply(self, obj, **kwargs):
+        bpy.context.scene.snow.height = 0.1
+        with butil.SelectObjects(obj):
+            bpy.ops.snow.create()
+            snow = bpy.context.active_object
+        tag_object(snow, "snow")
+        tag_object(snow, "boulder")
