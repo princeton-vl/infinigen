cfa910c6b9ecab12cb45f4d783bea9674f7b7506
Alexander Raistrick <araistrick@princeton.edu>
2023-06-30 03:11:08 -0400

    Add 10 lines last edited by Alexander Raistrick in worldgen/surfaces/scatters/jellyfish.py
    Commit made automatically to show authorship. This version of the code is not usable.

--- worldgen/surfaces/scatters/jellyfish.py
+++ worldgen/surfaces/scatters/jellyfish.py
@@ -1,19 +1,29 @@
 import numpy as np
+from numpy.random import uniform as U
 
 from assets.jellyfish import JellyfishFactory
 from nodes.node_wrangler import NodeWrangler
+from placement.factory import AssetFactory, make_asset_collection
+from placement.instance_scatter import scatter_instances
 
 
 def apply(obj, scale=1, density=1., n=10, selection=None):
     n_species = np.random.randint(2, 3)
     factories = list(JellyfishFactory(np.random.randint(1e5)) for i in range(n_species))
+    jellyfish = make_asset_collection(factories, name='jellyfish',
                                                  weights=np.random.uniform(0.5, 1, len(factories)), n=n,
                                                  verbose=True)
 
     def ground_offset(nw: NodeWrangler):
+        return nw.uniform(4 * scale, 8 * scale)
 
+    r = np.pi / 2
+    scatter_obj = scatter_instances(
         base_obj=obj, collection=jellyfish,
+        density=density, min_spacing=scale * 4,
         scale=scale, scale_rand=U(0.2, 0.9),
+        ground_offset=ground_offset, selection=selection,
         normal_fac=0.0,
         rotation_offset=lambda nw: nw.uniform(3*(-r,), 3*(r,)), reset_children=False,
+    )
     return scatter_obj, jellyfish
