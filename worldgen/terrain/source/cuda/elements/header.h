aa9bd8723d811e72f2a1f748008dc10dfe72a935
Zeyu Ma <zeyum@princeton.edu>
2023-06-30 03:11:42 -0400

    Add 22 lines last edited by Zeyu Ma in worldgen/terrain/source/cuda/elements/header.h
    Commit made automatically to show authorship. This version of the code is not usable.

---
+++ worldgen/terrain/source/cuda/elements/header.h
@@ -0,0 +1,22 @@
+using namespace std;
+#define DEVICE_FUNC __device__
+#define CONSTANT_ARRAY __device__ __constant__
+#include "../../common/utils/vectors.h"
+#include "../../common/utils/nodes_util.h"
+#include "../../common/utils/blender_noise.h"
+#include "../../common/utils/elements_util.h"
+#include "../../common/utils/FastNoiseLite.h"
+#include "../../common/utils/smooth_bool_ops.h"
+#include "../../common/nodes/node_shader_tex_noise.h"
+#include "../../common/nodes/node_shader_tex_voronoi.h"
+#include "../../common/nodes/node_shader_mix_rgb.h"
+#include "../../common/elements/caves.h"
+#include "../../common/elements/landtiles.h"
+#include "../../common/elements/ground.h"
+#include "../../common/elements/voronoi_rocks.h"
+#include "../../common/elements/warped_rocks.h"
+#include "../../common/elements/upsidedown_mountains.h"
+#include "../../common/elements/mountains.h"
+#include "../../common/elements/waterbody.h"
+#include "../../common/elements/atmosphere.h"
+#include "core.cu"
