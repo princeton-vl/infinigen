39a23ea9cce9dfae0517a3345fef2583c253e1b0
Lingjie Mei <lm5483@princeton.edu>
2023-06-30 03:11:13 -0400

    Add 36 lines last edited by Lingjie Mei in worldgen/assets/creatures/genome.py
    Commit made automatically to show authorship. This version of the code is not usable.

--- worldgen/assets/creatures/genome.py
+++ worldgen/assets/creatures/genome.py
@@ -13,26 +13,36 @@ from assets.creatures.creature_util import interp_dict
 
 from util.math import lerp
 
+
 @dataclass
 class IKParams:
     name: str
     chain_parts: int = 1 # how many parts up the hierarchy can this IK affect
     chain_length: int = None
     rotation_weight: float = 0
+    mode: str = 'iksolve'  # iksolve, pin
     target_size: float = 0.2
+
 
 @dataclass
 class Joint:
+    rest: np.array  # (3) float, degrees
     pose: np.array = None
+    bounds: np.array = None  # (2x3) float, degrees
+    stiffness: np.array = None  # (3) float
+    stretch: float = 0
 
     def __post_init__(self):
+        self.rest = np.array(self.rest, dtype=np.float)
         assert self.rest.shape == (3,), self.rest
 
         if self.pose is not None:
             self.pose = np.array(self.pose, dtype=np.float)
             assert self.pose.shape == (3,), self.pose
+
         if self.bounds is not None:
             self.bounds = np.array(self.bounds, dtype=np.float)
+
 
 @dataclass
 class Attachment:
@@ -47,13 +57,18 @@ class Attachment:
     def __post_init__(self):
         self.coord = np.array(self.coord, dtype=np.float)
 
+
 @dataclass
 class CreatureNode:
+    part_factory: typing.Any  # PartFactory
     att: Attachment
+
 
 @dataclass
 class CreatureGenome:
+    parts: Tree  # of CreatureNode
     postprocess_params: dict[dict] = field(default_factory=dict)
+
 
 def compute_child_matching(a: list[Tree], b: list[Tree]):
     def match_cost(a: Tree, b: Tree):
@@ -69,10 +84,12 @@ def compute_child_matching(a: list[Tree], b: list[Tree]):
 
     res = []
     for ai, bi in enumerate(perm):
+        res.append((a[ai] if ai != -1 else None, b[bi] if bi != -1 else None))
 
     return res
 
 
+def lerp_any(a, b, t):
     def cast(x):
         if isinstance(x, (tuple, list)):
             return np.array(x)
@@ -85,17 +102,22 @@ def compute_child_matching(a: list[Tree], b: list[Tree]):
 
     return res
 
+
 def interp_attachment(a: Attachment, b: Attachment, t: float):
     if a is None or b is None:
         return None
 
     s = b if t > 0.5 else a
 
+    joint = Joint(rest=lerp(a.joint.rest, b.joint.rest, t), bounds=s.joint.bounds)
 
+    att = Attachment(coord=lerp(a.coord, b.coord, t), joint=joint, bridge=s.bridge, side=s.side)
 
     return att
 
 
+def interp_creature_node(a: CreatureNode, b: CreatureNode, t):
+    s = b if t > 0.5 else a  # which of a,b should we take non-interpolatable things from
     fac = copy.copy(s.part_factory)
     fac.params = interp_dict(a.part_factory.params, b.part_factory.params, t, keys='switch', lerp=lerp_any)
 
@@ -103,6 +125,7 @@ def interp_attachment(a: Attachment, b: Attachment, t: float):
     att = a.att # TODO: Enable attachment interp later, debug symmetry
 
     return CreatureNode(part_factory=fac, att=att)
+
 
 def interp_part_tree(a: Tree, b: Tree, t: float):
     new_children = []
@@ -119,6 +142,8 @@ def interp_part_tree(a: Tree, b: Tree, t: float):
                 continue
         else:
             new_children.append(interp_part_tree(ac, bc, t))
+    return Tree(item=interp_creature_node(a.item, b.item, t), children=new_children)
+
 
 def interp_genome(a: CreatureGenome, b: CreatureGenome, t: float) -> CreatureGenome:
     assert 0 <= t and t <= 1
@@ -132,14 +157,25 @@ def interp_genome(a: CreatureGenome, b: CreatureGenome, t: float) -> CreatureGen
     #TODO a.postprocess_params
     postprocess = a.postprocess_params
 
+    return CreatureGenome(parts=interp_part_tree(a.parts, b.parts, t),
                           postprocess_params=postprocess)
 
 
+################
 # Syntactic sugar to make defining trees of part params less verbose
 ################
 
 def part(fac):
     return Tree(CreatureNode(fac, None))
 
+
+def attach(child: Tree, parent: Tree, coord=None, joint=None, bridge=None, side=1, rotation_basis='global',
+           bridge_rad=.0, smooth_rad=.0):
     assert child.item.att is None
+    if coord is None:
+        coord = np.array([0, 0, 0])
+    if joint is None:
+        joint = Joint((0, 0, 0))
+    child.item.att = Attachment(coord, joint, bridge, side, rotation_basis, bridge_rad, smooth_rad)
     parent.children.append(child)
+    return parent
