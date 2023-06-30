ab1e533feeccce09d0c7d88f35890bf1749c784c
Lingjie Mei <lm5483@princeton.edu>
2023-06-30 03:11:20 -0400

    Add 18 lines last edited by Lingjie Mei in worldgen/assets/deformed_trees/generate.py
    Commit made automatically to show authorship. This version of the code is not usable.

---
+++ worldgen/assets/deformed_trees/generate.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+from assets.deformed_trees import FallenTreeFactory, HollowTreeFactory, RottenTreeFactory
+from assets.deformed_trees.truncated import TruncatedTreeFactory
+from placement.factory import AssetFactory
+from util.math import FixedSeed
+
+class DeformedTreeFactory(AssetFactory):
+
+    def __init__(self, factory_seed, coarse=False):
+        super(DeformedTreeFactory, self).__init__(factory_seed, coarse)
+        self.makers = [FallenTreeFactory, RottenTreeFactory, TruncatedTreeFactory, HollowTreeFactory]
+        self.weights = np.array([1, 1, 1, 1])
+        with FixedSeed(factory_seed):
+            self.maker = np.random.choice(self.makers, p=self.weights / self.weights.sum())
+
+    def create_asset(self, **params):
+        return self.maker(**params)
