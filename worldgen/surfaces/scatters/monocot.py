86d40c7902fcaa9c28128d82d4293c44b75a1929
Lingjie Mei <lm5483@princeton.edu>
2023-06-30 03:11:08 -0400

    Add 9 lines last edited by Lingjie Mei in worldgen/surfaces/scatters/monocot.py
    Commit made automatically to show authorship. This version of the code is not usable.

--- worldgen/surfaces/scatters/monocot.py
+++ worldgen/surfaces/scatters/monocot.py
@@ -1,11 +1,19 @@
+import numpy as np
 from numpy.random import uniform as U
+
+from assets.monocot.generate import MonocotFactory
+from nodes.node_wrangler import NodeWrangler
+from placement.camera import ng_dist2camera
+from placement.instance_scatter import scatter_instances
 from placement.factory import AssetFactory, make_asset_collection
 from surfaces.scatters.utils.wind import wind
+
 def apply(obj, n=4, grass=None, selection=None, **kwargs):
  
     monocots = make_asset_collection(
         MonocotFactory(np.random.randint(1e5), grass=grass),  
         n=n, verbose=True, **kwargs)
+
     scatter_obj = scatter_instances(
         base_obj=obj, collection=monocots,
         vol_density=U(0.2, 4), min_spacing=0.1, 
@@ -14,3 +22,4 @@ def apply(obj, n=4, grass=None, selection=None, **kwargs):
         rotation_offset=wind(strength=20),
         normal_fac=0.3,
         selection=selection)
+    return scatter_obj, monocots
