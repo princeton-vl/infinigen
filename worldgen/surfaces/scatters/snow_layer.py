cb792d450731b109b66ef3ccb40f7827beaf74d4
Alexander Raistrick <araistrick@princeton.edu>
2023-06-30 03:11:08 -0400

    Add 13 lines last edited by Alexander Raistrick in worldgen/surfaces/scatters/snow_layer.py
    Commit made automatically to show authorship. This version of the code is not usable.

---
+++ worldgen/surfaces/scatters/snow_layer.py
@@ -0,0 +1,13 @@
+import bpy
+import mathutils
+
+from numpy.random import uniform, normal
+
+from nodes.node_wrangler import Nodes, NodeWrangler
+from surfaces import surface
+from util import blender as butil
+from nodes import node_utils
+
+from surfaces.templates import snow
+
+    
