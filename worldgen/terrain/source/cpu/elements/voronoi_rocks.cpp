123e6c40bc5ee29aeb717351e8fe1daadcc9c9fe
Zeyu Ma <zeyum@princeton.edu>
2023-06-30 03:11:44 -0400

    Add 21 lines last edited by Zeyu Ma in worldgen/terrain/source/cpu/elements/voronoi_rocks.cpp
    Commit made automatically to show authorship. This version of the code is not usable.

---
+++ worldgen/terrain/source/cpu/elements/voronoi_rocks.cpp
@@ -0,0 +1,21 @@
+extern "C" {
+
+    void call(
+        size_t size,
+        float3_nonbuiltin *positions,
+        float *sdfs,
+        float *auxs
+    ) {
+        using namespace data;
+        int n_auxiliaries = 2;
+        if (auxs == NULL) n_auxiliaries = 0;
+        #pragma omp parallel for
+        for (size_t idx = 0; idx < size; idx++) {
+            voronoi_rocks(
+                positions[idx], sdfs + idx, auxs + n_auxiliaries * idx, meta_param, second_meta_param,
+                d_i_params, d_f_params, second_d_i_params, second_d_f_params, third_d_i_params, third_d_f_params
+            );
+        }
+    }
+
+}
