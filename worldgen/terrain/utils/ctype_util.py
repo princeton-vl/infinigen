75bdaca30979d267e9016a9feb796b3ef0168b65
Zeyu Ma <zeyum@princeton.edu>
2023-06-30 03:11:36 -0400

    Add 22 lines last edited by Zeyu Ma in worldgen/terrain/utils/ctype_util.py
    Commit made automatically to show authorship. This version of the code is not usable.

---
+++ worldgen/terrain/utils/ctype_util.py
@@ -0,0 +1,22 @@
+import sys
+from ctypes import CDLL, POINTER, c_double, c_float, c_int32, RTLD_LOCAL
+from pathlib import Path
+
+
+# note: size of x should not exceed maximum
+def ASINT(x):
+    return x.ctypes.data_as(POINTER(c_int32))
+def ASDOUBLE(x):
+    return x.ctypes.data_as(POINTER(c_double))
+def ASFLOAT(x):
+    return x.ctypes.data_as(POINTER(c_float))
+
+def register_func(me, dll, name, argtypes=[], restype=None, caller_name=None):
+    if caller_name is None: caller_name = name
+    setattr(me, caller_name, getattr(dll, name))
+    func = getattr(me, caller_name)
+    func.argtypes = argtypes
+    func.restype = restype
+
+def load_cdll(path):
+    return CDLL(Path(sys.path[-1]) / path, mode=RTLD_LOCAL)
