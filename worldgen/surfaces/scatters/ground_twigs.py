4fbf52731c8a475cf33631c83f6f3512e9d360d6
Alexander Raistrick <araistrick@princeton.edu>
2023-06-30 03:11:07 -0400

    Add 33 lines last edited by Alexander Raistrick in worldgen/surfaces/scatters/ground_twigs.py
    Commit made automatically to show authorship. This version of the code is not usable.

---
+++ worldgen/surfaces/scatters/ground_twigs.py
@@ -0,0 +1,33 @@
+from random import random
+import bpy
+
+import numpy as np
+from numpy.random import uniform as U
+from mathutils import Vector
+
+from util import blender as butil
+
+from nodes import node_utils
+from placement.instance_scatter import scatter_instances
+from nodes.node_wrangler import Nodes, NodeWrangler
+from surfaces import surface
+
+from assets.trees.generate import make_twig_collection, random_species
+from .chopped_trees import approx_settle_transform
+
+def apply(obj, selection=None, n_leaf=0, n_twig=10, **kwargs):
+
+    (_, twig_params, leaf_params), _ = random_species(season='winter')
+    twigs = make_twig_collection(np.random.randint(1e5), twig_params, leaf_params, 
+        n_leaf=n_leaf, n_twig=n_twig, leaf_types=None, trunk_surface=surface.registry('bark'))
+
+    for o in twigs.objects:
+        approx_settle_transform(o, samples=40)
+
+    scatter_obj = scatter_instances(
+        base_obj=obj, collection=twigs,
+        scale=U(0.15, 0.3), scale_rand=U(0, 0.3), scale_rand_axi=U(0, 0.2),  
+        density=10, ground_offset=0.05,
+        selection=selection, taper_density=True)
+    
+    return scatter_obj, twigs
