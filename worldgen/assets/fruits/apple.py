e59f3926b5b94f7222f05ac4d13dc8daa96b76e9
Lahav Lipson <llipson@princeton.edu>
2023-06-30 03:11:28 -0400

    Add 1 lines last edited by Lahav Lipson in worldgen/assets/fruits/apple.py
    Commit made automatically to show authorship. This version of the code is not usable.

--- worldgen/assets/fruits/apple.py
+++ worldgen/assets/fruits/apple.py
@@ -22,6 +22,7 @@ from assets.fruits.general_fruit import FruitFactoryGeneralFruit
 class FruitFactoryApple(FruitFactoryGeneralFruit):
     def __init__(self, factory_seed, scale=1.0, coarse=False):
         super().__init__(factory_seed, scale=scale, coarse=coarse)
+        self.name = 'apple'
 
     def sample_cross_section_params(self, surface_resolution=256):
         return {
