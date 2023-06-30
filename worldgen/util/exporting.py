4f03597495d5bc9360984b55162d2c1cfca1d530
Yihan Wang <yw7685@princeton.edu>
2023-06-30 03:11:35 -0400

    Add 1 lines last edited by Yihan Wang in worldgen/util/exporting.py
    Commit made automatically to show authorship. This version of the code is not usable.

--- worldgen/util/exporting.py
+++ worldgen/util/exporting.py
@@ -28,6 +28,7 @@ def get_mesh_data(obj):
     vert_lookup = np.full((len(verts)*3,), np.nan, dtype=np.float32)
     verts.foreach_get("co", vert_lookup)
     vert_lookup = vert_lookup.reshape((-1, 3))
+    masktag = np.full(len(verts, ), 0, dtype=np.int32)
     if 'MaskTag' in obj.data.attributes:
         obj.data.attributes['MaskTag'].data.foreach_get("value", masktag)
     assert (loop_totals.size == 0) or (loop_totals.min() >= 0)
