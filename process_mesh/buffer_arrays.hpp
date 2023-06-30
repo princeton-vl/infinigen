8d6197cf3755ad019a0d08b43bd48da723d44ea5
Lahav Lipson <llipson@princeton.edu>
2023-06-30 03:10:59 -0400

    Add 117 lines last edited by Lahav Lipson in process_mesh/buffer_arrays.hpp
    Commit made automatically to show authorship. This version of the code is not usable.

---
+++ process_mesh/buffer_arrays.hpp
@@ -0,0 +1,117 @@
+struct InstanceID {
+    const int n1, n2;
+
+    // https://codereview.stackexchange.com/a/2608
+    long as_long() const {
+        const unsigned int n1ui = n1;
+        const unsigned int n2ui = n2;
+        return (((size_t) n2ui) << 32) | ((size_t) n1ui);
+    }
+};
+
+inline bool operator==(const InstanceID& lhs, const InstanceID& rhs)
+{
+    return lhs.as_long() == rhs.as_long();
+}
+
+template <>
+struct std::hash<InstanceID>
+{
+  std::size_t operator()(const InstanceID& k) const
+  {
+    return hash<int>()(k.as_long());
+  }
+};
+
+class npz {
+private:
+  cnpy::npz_t lookup;
+
+public:
+  fs::path path;
+
+  npz() {}
+
+  npz(const fs::path p) : path(p) {
+    assert_exists(path);
+    RASSERT(path.extension() == ".npz");
+    try {
+        lookup = cnpy::npz_load(path);
+    }
+    catch(const std::exception& e) {
+        throw std::runtime_error("Couldn't open " + path.string() + " - probably a BadZipFile error.");
+    }
+  }
+
+  bool check_key(const std::string &key) const {
+    return (lookup.count(key) != 0);
+  }
+
+    template<typename T>
+    std::vector<T> read_data(const std::string &key) const {
+        if (!check_key(key)){
+            std::cerr << "Key " << key << " not found in " << path << std::endl;
+            exit(1);
+        }
+        cnpy::NpyArray arr = lookup.at(key);
+        T* arr_ptr = arr.data<T>();
+        return std::vector<T>(arr_ptr, arr_ptr+arr.num_vals);
+    }
+
+};
+
+class BufferArrays
+{
+public:
+    std::vector<int> tag_lookup;
+    std::vector<float> lookup;
+    std::vector<unsigned int> indices;
+    std::unordered_map<InstanceID, Eigen::Matrix4f> model_mats;
+
+    BufferArrays(){}
+
+    BufferArrays(const npz &my_npz, std::string mesh_id, std::string obj_type);
+
+    size_t sizeof_instance() const {
+        return indices.size();
+    }
+
+    void clear(){
+        model_mats.clear();
+        RASSERT(is_empty());
+    }
+
+    bool is_empty() const {
+        return model_mats.empty();
+    }
+
+    std::vector<InstanceID> get_some_instance_ids(const size_t max_size=std::numeric_limits<size_t>::max()) const {
+        std::vector<InstanceID> keys;
+        for (const auto &kv : model_mats){
+            if ((keys.size() * sizeof_instance()) < max_size)
+                keys.push_back(kv.first);
+        }
+        return keys;
+    }
+
+    void remove_instances(const std::vector<InstanceID> &keys) {
+        for (const auto &key : keys){
+            const auto keyval = model_mats.find(key);
+            if (keyval != model_mats.end())
+                model_mats.erase(keyval);
+        }
+    }
+
+    std::vector<Eigen::Matrix4f> get_instances(const std::vector<InstanceID> &keys) const {
+        std::vector<Eigen::Matrix4f> matrix_worlds_subset;
+        for (const auto &key : keys){
+            const auto keyval = model_mats.find(key);
+            if (keyval != model_mats.end())
+                matrix_worlds_subset.push_back(keyval->second);
+            else
+                matrix_worlds_subset.push_back(Eigen::Matrix4f::Zero(4,4));
+        }
+        return matrix_worlds_subset;
+    }
+
+};
