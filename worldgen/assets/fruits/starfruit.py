d2cdf77edef61f136f73c9b942f8ba9bb00f9b87
Lahav Lipson <llipson@princeton.edu>
2023-06-30 03:11:28 -0400

    Add 1 lines last edited by Lahav Lipson in worldgen/assets/fruits/starfruit.py
    Commit made automatically to show authorship. This version of the code is not usable.

--- worldgen/assets/fruits/starfruit.py
+++ worldgen/assets/fruits/starfruit.py
@@ -22,6 +22,7 @@ from assets.fruits.general_fruit import FruitFactoryGeneralFruit
 class FruitFactoryStarfruit(FruitFactoryGeneralFruit):
     def __init__(self, factory_seed, scale=1.0, coarse=False):
         super().__init__(factory_seed, scale=scale, coarse=coarse)
+        self.name = 'starfruit'
 
     def sample_cross_section_params(self, surface_resolution=256):
         return {
