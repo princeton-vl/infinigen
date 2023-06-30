5406c39db68ff367c6ca6c1d66a936830f4f06ac
Lahav Lipson <llipson@princeton.edu>
2023-06-30 03:11:44 -0400

    Add 5 lines last edited by Lahav Lipson in worldgen/terrain/source/cpu/elements/upsidedown_mountains.cpp
    Commit made automatically to show authorship. This version of the code is not usable.

--- worldgen/terrain/source/cpu/elements/upsidedown_mountains.cpp
+++ worldgen/terrain/source/cpu/elements/upsidedown_mountains.cpp
@@ -11,10 +11,15 @@ extern "C" {
     void call(
         size_t size,
         float3_nonbuiltin *positions,
+        float *sdfs,
+        float *auxs
     ) {
         using namespace data;
+        int n_auxiliaries = 1;
+        if (auxs == NULL) n_auxiliaries = 0;
         #pragma omp parallel for
         for (size_t idx = 0; idx < size; idx++) {
+            upsidedown_mountains(positions[idx], sdfs + idx, auxs + n_auxiliaries * idx, d_i_params, d_f_params);
         }
     }
 
