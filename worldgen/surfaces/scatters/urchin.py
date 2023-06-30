cc425c917c3bcecd349cb3c0d9b849a29eb1c114
Alexander Raistrick <araistrick@princeton.edu>
2023-06-30 03:11:07 -0400

    Add 12 lines last edited by Alexander Raistrick in worldgen/surfaces/scatters/urchin.py
    Commit made automatically to show authorship. This version of the code is not usable.

--- worldgen/surfaces/scatters/urchin.py
+++ worldgen/surfaces/scatters/urchin.py
@@ -1,17 +1,29 @@
 import numpy as np
+from numpy.random import uniform as U
 
 from assets.urchin import UrchinFactory
 from nodes.node_wrangler import NodeWrangler
+from placement.factory import AssetFactory, make_asset_collection
 from placement.camera import ng_dist2camera
+from placement.instance_scatter import scatter_instances
 
 
+def apply(obj, n=5, selection=None):
     n_species = np.random.randint(2, 3)
     factories = list(UrchinFactory(np.random.randint(1e5)) for i in range(n_species))
+    urchin = make_asset_collection(factories, name='urchin',
                                               weights=np.random.uniform(0.5, 1, len(factories)), n=n,
                                               verbose=True)
+    
+    scale = U(0.1, 0.8)
+
     def ground_offset(nw: NodeWrangler):
         return nw.uniform(.4 * scale, .8 * scale)
 
+    scatter_obj = scatter_instances(
         base_obj=obj, collection=urchin,
+        vol_density=U(0.5, 2), ground_offset=ground_offset,
+        scale=scale, scale_rand=U(0.2, 0.4),
+        selection=selection)
 
     return scatter_obj, urchin
