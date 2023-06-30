1a318ea77d09e7184064e932e5b80c8492d45cd5
Lahav Lipson <llipson@princeton.edu>
2023-06-30 03:10:59 -0400

    Add 50 lines last edited by Lahav Lipson in process_mesh/show.py
    Commit made automatically to show authorship. This version of the code is not usable.

---
+++ process_mesh/show.py
@@ -0,0 +1,50 @@
+import argparse
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.text import Text
+from imageio import imread
+
+import numpy as np; np.random.seed(42)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_path', type=Path)
+    parser.add_argument('pos', type=int, nargs='*')
+    args = parser.parse_args()
+
+    if args.input_path.suffix == ".npy":
+        image = np.load(args.input_path)
+    else:
+        image = imread(args.input_path)
+
+    if len(args.pos) > 0:
+        assert len(args.pos) == 4
+        x1,y1,x2,y2 = args.pos
+        image = image[y1:y2+1, x1:x2+1]
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.imshow(image)
+
+    textvar: Text = ax.text(0, -15, "", style='italic')
+
+    def hover(event):
+        if event.xdata is not None:
+            x, y = round(event.xdata), round(event.ydata)
+            val = image[y,x]
+            if len(args.pos) > 0:
+                x += x1
+                y += y1
+            if val.dtype in [np.float32, np.float64]:
+                val = np.around(val, 3)
+            textvar.set_text(f"({x}, {y}) -> {val}")
+        else:
+            textvar.set_text("")
+        fig.canvas.draw_idle()
+
+    # add callback for mouse moves
+    fig.canvas.mpl_connect('motion_notify_event', hover)
+
+    plt.show()
