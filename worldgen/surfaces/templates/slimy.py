237ebd477d3926dc5564847a3415e23625485260
Alexander Raistrick <araistrick@princeton.edu>
2023-06-30 03:11:05 -0400

    Add 14 lines last edited by Alexander Raistrick in worldgen/surfaces/templates/slimy.py
    Commit made automatically to show authorship. This version of the code is not usable.

--- worldgen/surfaces/templates/slimy.py
+++ worldgen/surfaces/templates/slimy.py
@@ -1,12 +1,15 @@
 import os, sys
 import numpy as np
 import math as ma
+from surfaces.surface_utils import clip, sample_range, sample_ratio, sample_color, geo_voronoi_noise
 import bpy
 import mathutils
 from numpy.random import uniform, normal
+from nodes.node_wrangler import Nodes, NodeWrangler
 from surfaces import surface
 import random
 
+def shader_slimy(nw, rand=False, **input_kwargs):
     texture_coordinate = nw.new_node(Nodes.TextureCoord)
     
     value = nw.new_node(Nodes.Value)
@@ -18,6 +21,7 @@ import random
     musgrave_texture = nw.new_node(Nodes.MusgraveTexture,
         input_kwargs={'Vector': mapping, 'Scale': 6.4},
         attrs={'musgrave_dimensions': '4D'})
+    if rand:
         musgrave_texture.inputs["W"].default_value = sample_range(-5, 5)
 
     colorramp_1 = nw.new_node(Nodes.ColorRamp,
@@ -30,6 +34,7 @@ import random
     noise_texture_2 = nw.new_node(Nodes.NoiseTexture,
         input_kwargs={'Vector': mapping, 'Scale': 7.6, 'Distortion': 3.0},
         attrs={'noise_dimensions': '4D'})
+    if rand:
         noise_texture_2.inputs["W"].default_value = sample_range(-5, 5)
     
     colorramp_4 = nw.new_node(Nodes.ColorRamp,
@@ -44,12 +49,14 @@ import random
     
     principled_bsdf = nw.new_node(Nodes.PrincipledBSDF,
         input_kwargs={'Base Color': (0.6605, 0.0279, 0.0359, 1.0), 'Subsurface': 0.2, 'Subsurface Color': (0.4621, 0.0213, 0.0265, 1.0), 'Specular': 0.8591, 'Roughness': mix_1})
+    if rand:
         sample_color(principled_bsdf.inputs['Base Color'].default_value)
         sample_color(principled_bsdf.inputs['Subsurface Color'].default_value)
 
     material_output = nw.new_node(Nodes.MaterialOutput,
         input_kwargs={'Surface': principled_bsdf})
 
+def geo_slimy(nw, rand=False, **input_kwargs):
 
     group_input = nw.new_node(Nodes.GroupInput)
 
@@ -65,6 +72,7 @@ import random
     noise_texture = nw.new_node(Nodes.NoiseTexture,
         input_kwargs={'Vector': vector_math.outputs["Vector"], 'Distortion': 2.0},
         attrs={'noise_dimensions': '4D'})
+    if rand:
         noise_texture.inputs['W'].default_value = sample_range(-5, 5)
         noise_texture.inputs['Scale'].default_value = sample_range(3, 7)
         noise_texture.inputs['Distortion'].default_value = sample_range(1, 4)
@@ -90,17 +98,23 @@ import random
         attrs={'operation': 'MULTIPLY'})
     
     value_1 = nw.new_node(Nodes.Value)
+    value_1.outputs["Value"].default_value = uniform(0.005, 0.02) if rand else 0.015
 
     vector_math_2 = nw.new_node(Nodes.VectorMath,
         input_kwargs={0: vector_math_1.outputs["Vector"], 1: value_1},
         attrs={'operation': 'MULTIPLY'})
     
     set_position = nw.new_node(Nodes.SetPosition,
+        input_kwargs={'Geometry': group_input, 'Offset': vector_math_2.outputs["Vector"]})
     
     capture_attribute = nw.new_node(Nodes.CaptureAttribute,
+        input_kwargs={
+            'Geometry': set_position, 
         attrs={'data_type': 'FLOAT_VECTOR'})
     
     group_output = nw.new_node(Nodes.GroupOutput,
         input_kwargs={'Geometry': capture_attribute.outputs["Geometry"], 'Attribute': capture_attribute.outputs["Attribute"]})
 
+def apply(obj, geo_kwargs=None, shader_kwargs=None, **kwargs):
+    surface.add_geomod(obj, geo_slimy, apply=False, input_kwargs=geo_kwargs, attributes=['offset'])
     surface.add_material(obj, shader_slimy, reuse=False, input_kwargs=shader_kwargs)
