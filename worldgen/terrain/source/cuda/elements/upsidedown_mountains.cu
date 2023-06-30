6ae6b46fe75fb234e4d65f55371e4d2d32e56a18
Lahav Lipson <llipson@princeton.edu>
2023-06-30 03:11:42 -0400

    Add 12 lines last edited by Lahav Lipson in worldgen/terrain/source/cuda/elements/upsidedown_mountains.cu
    Commit made automatically to show authorship. This version of the code is not usable.

--- worldgen/terrain/source/cuda/elements/upsidedown_mountains.cu
+++ worldgen/terrain/source/cuda/elements/upsidedown_mountains.cu
@@ -2,11 +2,15 @@ __global__ void upsidedown_mountains_kernel(
     size_t size,
     float3_nonbuiltin *position,
     float *sdfs,
+    float *auxs,
     int *i_params,
     float *f_params
 ) {
     size_t idx = blockIdx.x * blockDim.x + threadIdx.x;
     if (idx < size) {
+        int n_auxiliaries = 1;
+        if (auxs == NULL) n_auxiliaries = 0;
+        upsidedown_mountains(position[idx], sdfs + idx, auxs + idx * n_auxiliaries, i_params, f_params);
     }
 }
 
@@ -16,15 +20,23 @@ extern "C" {
     void call(
         size_t size,
         float3_nonbuiltin *positions,
+        float *sdfs,
+        float *auxs
     ) {
         using namespace data;
+        const int n_auxiliaries = 1;
         float3_nonbuiltin *d_positions;
         cudaMalloc((void **)&d_positions, size * sizeof(float3_nonbuiltin));
         cudaMemcpy(d_positions, positions, size * sizeof(float3_nonbuiltin), cudaMemcpyHostToDevice);
         float *d_sdfs;
         cudaMalloc((void **)&d_sdfs, size * sizeof(float));
+        float *d_auxs=NULL;
+        if (auxs != NULL) cudaMalloc((void **)&d_auxs, size * n_auxiliaries * sizeof(float));
+        upsidedown_mountains_kernel<<<ceil(size / 256.0), 256>>>(size, d_positions, d_sdfs, d_auxs, d_i_params, d_f_params);
         cudaMemcpy(sdfs, d_sdfs, size * sizeof(float), cudaMemcpyDeviceToHost);
+        if (auxs != NULL) cudaMemcpy(auxs, d_auxs, size * n_auxiliaries * sizeof(float), cudaMemcpyDeviceToHost);
         cudaFree(d_positions);
+        if (auxs != NULL) cudaFree(d_auxs);
         cudaFree(d_sdfs);
     }
 
