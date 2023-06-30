14b3bccc3beb1168dc1b92631c7f3782a99f09af
Lingjie Mei <lm5483@princeton.edu>
2023-06-30 03:11:22 -0400

    Add 6 lines last edited by Lingjie Mei in worldgen/assets/cactus/__init__.py
    Commit made automatically to show authorship. This version of the code is not usable.

---
+++ worldgen/assets/cactus/__init__.py
@@ -0,0 +1,6 @@
+from .generate import CactusFactory, ColumnarCactusFactory, GlobularCactusFactory, PrickyPearCactusFactory, \
+    KalidiumCactusFactory
+from .columnar import ColumnarBaseCactusFactory
+from .globular import GlobularBaseCactusFactory
+from .pricky_pear import PrickyPearBaseCactusFactory
+from .kalidium import KalidiumBaseCactusFactory
