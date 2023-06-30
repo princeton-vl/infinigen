7a5ef91f137828efb45b31a1ea0458d12063cb40
Lingjie Mei <lm5483@princeton.edu>
2023-06-30 03:11:32 -0400

    Add 35 lines last edited by Lingjie Mei in worldgen/tools/results/strip_alpha_background.py
    Commit made automatically to show authorship. This version of the code is not usable.

---
+++ worldgen/tools/results/strip_alpha_background.py
@@ -0,0 +1,35 @@
+import imageio
+from pathlib import Path
+import argparse
+import pdb
+
+parser = argparse.ArgumentParser()
+parser.add_argument('folder', type=Path, nargs='+')
+parser.add_argument('thresh', type=float, default=0.05)
+
+
+def main(thresh, folder):
+    assert thresh > 1, f'Images are 0-255 you probably didnt want {thresh=}'
+
+    for folder in folder:
+        out_folder = folder.parent / (folder.stem + f'_thresh_{thresh}')
+        out_folder.mkdir(exist_ok=True, parents=True)
+
+        for imgpath in folder.iterdir():
+            try:
+                img = imageio.imread(imgpath)
+            except:
+                continue
+
+            pixs = img.reshape(-1, 4)
+            mask = pixs[:, -1] < thresh
+            pixs[mask] = 0
+            img = pixs.reshape(img.shape)
+
+            print(f'Stripped {100 * mask.mean()}% from {imgpath}')
+            imageio.imwrite(out_folder / imgpath.name, img)
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    main(args.thresh, args.folder)
