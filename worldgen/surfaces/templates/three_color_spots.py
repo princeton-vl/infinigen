5f6e7a46447a82ece85257bd78043f850a5f9280
Alexander Raistrick <araistrick@princeton.edu>
2023-06-30 03:11:04 -0400

    Add 18 lines last edited by Alexander Raistrick in worldgen/surfaces/templates/three_color_spots.py
    Commit made automatically to show authorship. This version of the code is not usable.

--- worldgen/surfaces/templates/three_color_spots.py
+++ worldgen/surfaces/templates/three_color_spots.py
@@ -1,11 +1,14 @@
 import os, sys
 import numpy as np
 import math as ma
+from surfaces.surface_utils import clip, sample_range, sample_ratio, sample_color, geo_voronoi_noise
 import bpy
 import mathutils
 from numpy.random import uniform, normal
+from nodes.node_wrangler import Nodes, NodeWrangler
 from surfaces import surface
 
+def shader_spot(nw, rand=True, **input_kwargs):
 
     texture_coordinate = nw.new_node(Nodes.TextureCoord)
     
@@ -14,6 +17,7 @@ from surfaces import surface
     
     mix = nw.new_node(Nodes.MixRGB,
         input_kwargs={'Fac': 0.7, 'Color1': noise_texture.outputs["Color"], 'Color2': texture_coordinate.outputs["Object"]})
+    if rand:
         mix.inputs["Fac"].default_value = sample_range(0.5, 0.9)
 
     scale = nw.new_node(Nodes.Value)
@@ -21,6 +25,7 @@ from surfaces import surface
     
     mapping = nw.new_node(Nodes.Mapping,
         input_kwargs={'Vector': mix, 'Scale': scale})
+    if rand:
         for i in range(3):
             mapping.inputs['Location'].default_value[i] = sample_range(-1, 1)
             mapping.inputs['Rotation'].default_value[i] = sample_range(0, 2*ma.pi)
@@ -36,14 +41,17 @@ from surfaces import surface
     
     mapping_1 = nw.new_node(Nodes.Mapping,
         input_kwargs={'Vector': mix_7})
+    if rand:
         for i in range(3):
             mapping_1.inputs['Scale'].default_value[i] = sample_range(0.8, 1.2)
     
     spot2_size = nw.new_node(Nodes.Value)
+    spot2_size.outputs["Value"].default_value = sample_range(1, 3) if rand else 1.5
 
     voronoi_texture_2 = nw.new_node(Nodes.VoronoiTexture,
         input_kwargs={'Vector': mapping_1, 'Scale': spot2_size},
         attrs={'voronoi_dimensions': '4D'})
+    if rand:
         voronoi_texture_2.inputs['W'].default_value = sample_range(-5, 5)
 
     math_4 = nw.new_node(Nodes.Math,
@@ -58,6 +66,7 @@ from surfaces import surface
     colorramp_1.color_ramp.elements[1].color = (0.0563, 0.0563, 0.0563, 1.0)
     colorramp_1.color_ramp.elements[2].position = 0.5227
     colorramp_1.color_ramp.elements[2].color = (1.0, 1.0, 1.0, 1.0)
+    if rand:
         colorramp_1.color_ramp.elements[1].position = sample_range(0.18, 0.23)
 
     math_2 = nw.new_node(Nodes.Math,
@@ -66,6 +75,7 @@ from surfaces import surface
     voronoi_texture_3 = nw.new_node(Nodes.VoronoiTexture,
         input_kwargs={'Vector': mapping_1, 'Scale': math_2},
         attrs={'voronoi_dimensions': '4D'})
+    if rand:
         voronoi_texture_3.inputs['W'].default_value = sample_range(-5, 5)
 
     math_3 = nw.new_node(Nodes.Math,
@@ -86,14 +96,17 @@ from surfaces import surface
     
     mapping_2 = nw.new_node(Nodes.Mapping,
         input_kwargs={'Vector': mix_3})
+    if rand:
         for i in range(3):
             mapping_2.inputs['Scale'].default_value[i] = sample_range(0.8, 1.2)
 
     spot1_size = nw.new_node(Nodes.Value)
+    spot1_size.outputs["Value"].default_value = sample_range(1, 3) if rand else 1.5
 
     voronoi_texture = nw.new_node(Nodes.VoronoiTexture,
         input_kwargs={'Vector': mapping_2, 'W': 1.0, 'Scale': spot1_size},
         attrs={'voronoi_dimensions': '4D'})
+    if rand:
         voronoi_texture.inputs['W'].default_value = sample_range(-5, 5)
 
     math_5 = nw.new_node(Nodes.Math,
@@ -108,13 +121,16 @@ from surfaces import surface
     colorramp_2.color_ramp.elements[1].color = (0.4798, 0.4798, 0.4798, 1.0)
     colorramp_2.color_ramp.elements[2].position = 0.5136
     colorramp_2.color_ramp.elements[2].color = (1.0, 1.0, 1.0, 1.0)
+    if rand:
         colorramp_2.color_ramp.elements[1].position = sample_range(0.18, 0.32)
 
     value = nw.new_node(Nodes.Value)
+    value.outputs["Value"].default_value = sample_range(2, 8) if rand else 5
 
     voronoi_texture_1 = nw.new_node(Nodes.VoronoiTexture,
         input_kwargs={'Vector': mapping_2, 'Scale': value},
         attrs={'voronoi_dimensions': '4D'})
+    if rand:
         voronoi_texture_1.inputs['W'].default_value = sample_range(-5, 5)
 
     math_6 = nw.new_node(Nodes.Math,
@@ -158,4 +174,6 @@ from surfaces import surface
     material_output = nw.new_node(Nodes.MaterialOutput,
         input_kwargs={'Surface': principled_bsdf})
 
+def apply(obj, geo_kwargs=None, shader_kwargs=None, **kwargs):
+    surface.add_geomod(obj, geo_voronoi_noise, apply=False, input_kwargs=geo_kwargs, attributes=['offset'])
     surface.add_material(obj, shader_spot, reuse=False, input_kwargs=shader_kwargs)
