73c750c1816ba0524d01b7dcb63be6c8497dcebf
Zeyu Ma <zeyum@princeton.edu>
2023-06-30 03:11:44 -0400

    Add 17 lines last edited by Zeyu Ma in worldgen/terrain/source/cpu/elements/atmosphere.cpp
    Commit made automatically to show authorship. This version of the code is not usable.

---
+++ worldgen/terrain/source/cpu/elements/atmosphere.cpp
@@ -0,0 +1,17 @@
+extern "C" {
+    void call(
+        size_t size,
+        float3_nonbuiltin *positions,
+        float *sdfs
+    ) {
+        using namespace data;
+        #pragma omp parallel for
+        for (size_t idx = 0; idx < size; idx++) {
+            atmosphere(
+                positions[idx], sdfs + idx, meta_param,
+                d_i_params, d_f_params, second_d_i_params, second_d_f_params
+            );
+
+        }
+    }
+}
