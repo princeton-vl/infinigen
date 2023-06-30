6ab70586e4c9cad96dd277d5cf784b2d78531940
Alexander Raistrick <araistrick@princeton.edu>
2023-06-30 03:11:03 -0400

    Add 23 lines last edited by Alexander Raistrick in worldgen/surfaces/templates/bark_random.py
    Commit made automatically to show authorship. This version of the code is not usable.

--- worldgen/surfaces/templates/bark_random.py
+++ worldgen/surfaces/templates/bark_random.py
@@ -7,6 +7,7 @@ from nodes import node_utils
 from surfaces import surface
 import numpy as np
 
+from util.math import FixedSeed
 
 @node_utils.to_nodegroup('nodegroup_calc_radius', singleton=True, type='GeometryNodeTree')
 def nodegroup_calc_radius(nw):
@@ -57,8 +58,10 @@ def nodegroup_shader_canonical_coord(nw):
             ('NodeSocketVector', 'Vector', (0.0, 0.0, 0.0))])
     
     attribute = nw.new_node(Nodes.Attribute,
+        attrs={'attribute_name': 'parent_skeleton_loc'})
     
     attribute_1 = nw.new_node(Nodes.Attribute,
+        attrs={'attribute_name': 'skeleton_loc'})
     
     subtract = nw.new_node(Nodes.VectorMath,
         input_kwargs={0: attribute_1.outputs["Vector"], 1: attribute.outputs["Vector"]},
@@ -368,8 +371,10 @@ def nodegroup_apply_geo_matv2(nw):
     group_output = nw.new_node(Nodes.GroupOutput,
         input_kwargs={'Geometry': set_material})
 
+def shader_random_bark_mat(nw, base_color:Tuple, geo_params, selection=None):
     
     multiply = nw.new_node(Nodes.Math,
+        input_kwargs={0: geo_params['Noise Texture Scale'], 1: 4.0},
         attrs={'operation': 'MULTIPLY'})
     
     noise_texture_1 = nw.new_node(Nodes.NoiseTexture,
@@ -385,6 +390,7 @@ def nodegroup_apply_geo_matv2(nw):
         input_kwargs={'Vector': attribute_5.outputs["Vector"]})
     
     multiply_1 = nw.new_node(Nodes.Math,
+        input_kwargs={0: geo_params['Noise Texture Scale'], 1: 2.0},
         attrs={'operation': 'MULTIPLY'})
     
     noise_texture = nw.new_node(Nodes.NoiseTexture,
@@ -394,13 +400,16 @@ def nodegroup_apply_geo_matv2(nw):
         input_kwargs={'Value': noise_texture.outputs["Fac"], 1: 0.35, 2: 0.4, 3: 0.5, 4: 0.0})
     
     group_2 = nw.new_node(nodegroup_voronoi().name,
+        input_kwargs={'Coordinate': group_canonical, 'Texture Scale': geo_params['Secondary Voronoi Scale']})
     
     map_range = nw.new_node(Nodes.MapRange,
         input_kwargs={'Value': group_2, 3: 0.7, 4: 0.0})
     
     group = nw.new_node(nodegroup_inject_z_noise_and_scale().name,
+        input_kwargs={'Coordinate': group_canonical, 'Noise Scale': geo_params['Z Noise Scale'], 'Noise Amount': geo_params['Z Noise Amount'], 'Z Multiplier': geo_params['Z Multiplier']})
     
     group_1 = nw.new_node(nodegroup_voronoi().name,
+        input_kwargs={'Coordinate': group, 'Texture Scale': geo_params['Primary Voronoi Scale'], 'Randomness': geo_params['Primary Voronoi Randomness']})
     
     rgb_1 = nw.new_node(Nodes.RGB)
     rgb_1.outputs[0].default_value = base_color
@@ -437,6 +446,7 @@ def nodegroup_apply_geo_matv2(nw):
     material_output = nw.new_node(Nodes.MaterialOutput,
         input_kwargs={'Surface': principled_bsdf})
 
+def geo_bark_random(nw, base_color, geo_params, selection=None):
     group_input = nw.new_node(Nodes.GroupInput,
         expose_input=[('NodeSocketGeometry', 'Geometry', None),
             ('NodeSocketFloat', 'Displacement Scale', geo_params['Displacement Scale']),
@@ -451,6 +461,8 @@ def nodegroup_apply_geo_matv2(nw):
             ('NodeSocketFloat', 'Noise Texture Detail', geo_params['Noise Texture Detail']),
             ('NodeSocketFloat', 'Noise Texture Weight', geo_params['Noise Texture Weight'])])
 
+    parent_loc = nw.new_node(Nodes.NamedAttribute, ['parent_skeleton_loc'], attrs={'data_type': 'FLOAT_VECTOR'})
+    skeleton_loc = nw.new_node(Nodes.NamedAttribute, ['skeleton_loc'], attrs={'data_type': 'FLOAT_VECTOR'})
     
     position = nw.new_node(Nodes.InputPosition)
     
@@ -459,11 +471,13 @@ def nodegroup_apply_geo_matv2(nw):
         attrs={'data_type': 'FLOAT_VECTOR'})
     
     canonicalcoord = nw.new_node(nodegroup_canonical_coord().name,
+        input_kwargs={'Self Location': skeleton_loc, 'Parent Location': parent_loc})
 
     group_1 = nw.new_node(nodegroup_random_bark_geo().name,
         input_kwargs={'Position': canonicalcoord, 'Noise Scale': group_input.outputs["Z Noise Scale"], 'Noise Amount': group_input.outputs["Z Noise Amount"], 3: group_input.outputs["Primary Voronoi Scale"], 'Randomness': group_input.outputs["Primary Voronoi Randomness"], 5: group_input.outputs["Displacement Scale"], 'Mix Weight': group_input.outputs["Secondary Voronoi Mix Weight"], 'Scale': group_input.outputs["Noise Texture Scale"], 'Detail': group_input.outputs["Noise Texture Detail"], 9: group_input.outputs["Noise Texture Weight"], 'Z Multiplier': group_input.outputs["Z Multiplier"], 11: group_input.outputs["Secondary Voronoi Scale"]})
 
     calc_radius = nw.new_node(nodegroup_calc_radius().name,
+        input_kwargs={'Self Location': skeleton_loc, 'Parent Location': parent_loc})
 
     multiply = nw.new_node(Nodes.Math,
         input_kwargs={0: calc_radius, 1: 3.0},
@@ -473,8 +487,12 @@ def nodegroup_apply_geo_matv2(nw):
         input_kwargs={0: multiply, 1: group_1},
         attrs={'operation': 'MULTIPLY'})
     group = nw.new_node(nodegroup_apply_geo_matv2().name,
+        input_kwargs={'Geometry': capture_attribute.outputs["Geometry"], 
+                      'Displacement Amount': nw.multiply(multiply_1, surface.eval_argument(nw, selection)), 
+                      'Displacement Scale': 0.5, 'Material': surface.shaderfunc_to_material(shader_random_bark_mat, geo_params=geo_params, base_color=base_color)})
     
     group_output = nw.new_node(Nodes.GroupOutput,
+        input_kwargs={'Geometry': group, 'initial_position': capture_attribute.outputs["Attribute"]})
 
 # https://blender.stackexchange.com/questions/158896/how-set-hex-in-rgb-node-python
 def srgb_to_linearrgb(c):
@@ -517,3 +535,8 @@ def get_random_bark_params(seed):
     return geo_params, color_params
 
 def apply(obj, selection=None, **kwargs):
+
+    geo_params, color_params = get_random_bark_params(seed=np.random.randint(1e5))
+
+    surface.add_geomod(obj, geo_bark_random, selection=selection, 
+        input_kwargs={'base_color': color_params['Color'], 'geo_params': geo_params}, attributes=['initial_position'])
