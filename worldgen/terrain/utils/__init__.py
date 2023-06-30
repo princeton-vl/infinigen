de0ea76dc9044be143620b623ce4376355bdca4c
Zeyu Ma <zeyum@princeton.edu>
2023-06-30 03:11:36 -0400

    Add 17 lines last edited by Zeyu Ma in worldgen/terrain/utils/__init__.py
    Commit made automatically to show authorship. This version of the code is not usable.

---
+++ worldgen/terrain/utils/__init__.py
@@ -0,0 +1,17 @@
+from .mesh import Mesh, write_attributes, Vars, move_modifier
+from .ctype_util import ASINT, ASDOUBLE, ASFLOAT, register_func, load_cdll
+from .logging import Timer
+from .camera import get_caminfo
+from .image_processing import (
+    boundary_smooth, smooth, read, sharpen, grid_distance, get_normal
+)
+from .random import perlin_noise, chance, drive_param, random_int, random_int_large
+
+from .kernelizer_util import (
+    ATTRTYPE_DIMS, ATTRTYPE_FIELDS, ATTRTYPE_NP, NODE_ATTRS_AVAILABLE,
+    AttributeType, FieldsType, Nodes, SocketType, KernelDataType,
+    SurfaceTypes, usable_name, SOCKETTYPE_KERNEL, sanitize, special_sanitize,
+    special_sanitize_float_curve, NODE_FUNCTIONS, concat_string, var_list,
+    value_string, collecting_vars, get_imp_var_name, special_sanitize_constant,
+    KERNELDATATYPE_NPTYPE, KERNELDATATYPE_DIMS
+)
