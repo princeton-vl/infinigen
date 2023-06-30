7cd2fd5e127a043a28ec216c43097685a6302e86
Alexander Raistrick <araistrick@princeton.edu>
2023-06-30 03:11:04 -0400

    Add 47 lines last edited by Alexander Raistrick in worldgen/surfaces/templates/tiger_attr.py
    Commit made automatically to show authorship. This version of the code is not usable.

--- worldgen/surfaces/templates/tiger_attr.py
+++ worldgen/surfaces/templates/tiger_attr.py
@@ -4,18 +4,32 @@ import math as ma
 from surfaces.surface_utils import clip, sample_range, sample_ratio, sample_color, geo_voronoi_noise
 import bpy
 import mathutils
+from numpy.random import uniform as U, normal as N, randint
+from nodes.node_wrangler import Nodes, NodeWrangler
+from nodes import node_utils
+from nodes.color import color_category, hsv2rgba
+from surfaces import surface
+
+from assets.creatures.nodegroups.shader import nodegroup_color_mask
+import bpy
+import mathutils
 from numpy.random import uniform, normal, randint
 from nodes.node_wrangler import Nodes, NodeWrangler
 from nodes import node_utils
 from nodes.color import color_category
 from surfaces import surface
 
+from assets.creatures.nodegroups.shader import nodegroup_color_mask
 
+@node_utils.to_nodegroup('nodegroup_tiger_fac', singleton=False, type='ShaderNodeTree')
+def nodegroup_tiger_fac(nw: NodeWrangler):
     # Code generated using version 2.6.4 of the node_transpiler
 
     group_input = nw.new_node(Nodes.GroupInput, expose_input=[('NodeSocketVector', 'Vector', (0.0000, 0.0000, 0.0000))])
     
+    noise_texture = nw.new_node(Nodes.NoiseTexture,
         input_kwargs={'Vector': group_input.outputs["Vector"]},
+        attrs={'noise_dimensions': '4D'})
     noise_texture.inputs["W"].default_value = uniform(-10, 10)
     
     mix_3 = nw.new_node(Nodes.MixRGB,
@@ -27,10 +41,14 @@ from surfaces import surface
         attrs={'musgrave_dimensions': '4D'})
     musgrave_texture_1.inputs["W"].default_value = uniform(-10, 10)
     
+    value_1 = nw.new_node(Nodes.Value)
     value_1.outputs[0].default_value = normal(0.1180, 0.0100)
+    
+    value_2 = nw.new_node(Nodes.Value)
     value_2.outputs[0].default_value = normal(0.0600, 0.0100)
     
     add = nw.new_node(Nodes.Math, input_kwargs={0: value_1, 1: value_2})
+    
     greater_than = nw.new_node(Nodes.Math, input_kwargs={0: musgrave_texture_1, 1: add}, attrs={'operation': 'GREATER_THAN'})
     
     subtract = nw.new_node(Nodes.Math, input_kwargs={0: value_1, 1: value_2}, attrs={'operation': 'SUBTRACT'})
@@ -38,6 +56,7 @@ from surfaces import surface
     greater_than_1 = nw.new_node(Nodes.Math, input_kwargs={0: musgrave_texture_1, 1: subtract}, attrs={'operation': 'GREATER_THAN'})
     
     less_than = nw.new_node(Nodes.Math, input_kwargs={0: greater_than, 1: greater_than_1}, attrs={'operation': 'LESS_THAN'})
+    
     colorramp_3 = nw.new_node(Nodes.ColorRamp, input_kwargs={'Fac': musgrave_texture_1})
     colorramp_3.color_ramp.interpolation = "CONSTANT"
     colorramp_3.color_ramp.elements[0].position = 0.0000
@@ -69,20 +88,46 @@ from surfaces import surface
     group_output = nw.new_node(Nodes.GroupOutput, input_kwargs={'Color': mix_2}, attrs={'is_active_output': True})
 
 
+def shader_tiger_attr(nw: NodeWrangler):
+    # Code generated using version 2.4.3 of the node_transpiler
 
+    local_pos = nw.new_node(Nodes.Attribute,
+        attrs={'attribute_name': 'local_pos'})
     
+    value = nw.new_node(Nodes.Value)
+    value.outputs[0].default_value = U(2, 4)
     
+    attribute = nw.new_node(Nodes.Attribute,
+        attrs={'attribute_name': 'tag_head'})
     
+    multiply = nw.new_node(Nodes.Math,
+        input_kwargs={0: attribute.outputs["Fac"], 1: N(3, 1)},
         attrs={'operation': 'MULTIPLY'})
     
+    add = nw.new_node(Nodes.Math,
+        input_kwargs={0: value, 1: multiply})
     
+    scale = nw.new_node(Nodes.VectorMath,
+        input_kwargs={0: local_pos.outputs["Vector"], 'Scale': add},
+        attrs={'operation': 'SCALE'})
     
+    group_1 = nw.new_node(nodegroup_tiger_fac().name,
+        input_kwargs={'Vector': scale.outputs["Vector"]})
     
+    group = nw.new_node(nodegroup_color_mask().name)
     
     colorramp = nw.new_node(Nodes.ColorRamp,
+        input_kwargs={'Fac': group})
     colorramp.color_ramp.elements[0].position = 0.0
+    colorramp.color_ramp.elements[0].color = hsv2rgba((U(0.015, 0.06), U(0.85, 0.95), U(0.15, 0.7)))
     colorramp.color_ramp.elements[1].position = 1.0
+    colorramp.color_ramp.elements[1].color = hsv2rgba((U(0.02, 0.05), U(0.3, 0.7), U(0.15, 0.7)))
+    
     mix_5 = nw.new_node(Nodes.MixRGB,
+        input_kwargs={
+            'Fac': group_1, 
+            'Color1': (0.01, 0.01, 0.01, 1.0), 
+            'Color2': colorramp.outputs["Color"]})
     
     principled_bsdf = nw.new_node(Nodes.PrincipledBSDF,
         input_kwargs={'Base Color': mix_5, 'Specular': 0.0},
@@ -91,6 +136,8 @@ from surfaces import surface
     material_output = nw.new_node(Nodes.MaterialOutput,
         input_kwargs={'Surface': principled_bsdf})
 
+def apply(obj, selection=None, **kwargs):
+    surface.add_material(obj, shader_tiger_attr, selection=selection)
 
 if __name__ == "__main__":
     for i in range(1):
