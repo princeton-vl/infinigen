c20a33b4059e331092f091d750b0a260e74f2bfd
Zeyu Ma <zeyum@princeton.edu>
2023-06-30 03:11:30 -0400

    Add 43 lines last edited by Zeyu Ma in worldgen/tools/dev/params_parser.py
    Commit made automatically to show authorship. This version of the code is not usable.

---
+++ worldgen/tools/dev/params_parser.py
@@ -0,0 +1,43 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', '--file_path', type=str)
+args = parser.parse_args()
+
+output = ""
+current_type = ""
+current_vars = []
+
+code = ""
+
+def get_code(current_type, variables):
+    code = ""
+    for i, v in enumerate(variables):
+        code += f"    {current_type} {v} = {current_type[0]}_params[{i}];\n"
+    return code
+
+with open(args.file_path, "r") as f:
+    lines = f.readlines()
+    i = 0
+    while i < len(lines):
+        line = lines[i]
+        if line.lstrip().startswith("/* params"):
+            while True:
+                i += 1
+                if lines[i].rstrip().endswith(":"):
+                    if current_type != "":
+                        code += get_code(current_type, current_vars)
+                    if lines[i].lstrip().startswith("int"):
+                        current_type = "int"
+                        current_vars = []
+                    elif lines[i].lstrip().startswith("float"):
+                        current_type = "float"
+                        current_vars = []
+                elif lines[i].rstrip().endswith("*/"):
+                    code += get_code(current_type, current_vars)
+                    break
+                else:
+                    current_vars.extend([x.lstrip().rstrip() for x in lines[i].lstrip().rstrip().rstrip(',').split(",") if x.lstrip().rstrip() != ""])
+        i += 1
+                        
+print(code)
