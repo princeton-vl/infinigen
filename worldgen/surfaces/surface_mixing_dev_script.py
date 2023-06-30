3a2fc31dea210913b544d3d0aaab709ccacb072c
Alexander Raistrick <araistrick@princeton.edu>
2023-06-30 03:11:03 -0400

    Add 12 lines last edited by Alexander Raistrick in worldgen/surfaces/surface_mixing_dev_script.py
    Commit made automatically to show authorship. This version of the code is not usable.

--- worldgen/surfaces/surface_mixing_dev_script.py
+++ worldgen/surfaces/surface_mixing_dev_script.py
@@ -18,11 +18,23 @@ import bpy
 import gin
 import mathutils
 from numpy.random import uniform, normal
+from nodes.node_wrangler import Nodes, NodeWrangler
 from surfaces import surface
+from surfaces.templates import dirt, cobble_stone, cracked_ground, sand, mountain
 import generate # Necessary to parse gin config files
+
+gin.clear_config()
+gin.enter_interactive_mode()
 gin.parse_config_files_and_bindings(["config/base.gin"], [])
 
 def right_half(nw: NodeWrangler, **kwargs):
+    x = nw.new_node(Nodes.SeparateXYZ, [nw.new_node(Nodes.InputPosition)])
+    return nw.new_node(Nodes.Math, [(x, "Y"), 0], attrs={"operation": "GREATER_THAN"})
 
 def left_half(nw: NodeWrangler, **kwargs):
+    x = nw.new_node(Nodes.SeparateXYZ, [nw.new_node(Nodes.InputPosition)])
+    return nw.new_node(Nodes.Math, [(x, "Y"), 0], attrs={"operation": "LESS_THAN"})
 
+obj = bpy.context.active_object
+cracked_ground.apply(obj, selection=surface.write_attribute(obj, right_half))
+dirt.apply(obj, selection=surface.write_attribute(obj, left_half))
