f0ca98d8599378d01d9bce66d280a08dc81f1022
Lingjie Mei <lm5483@princeton.edu>
2023-06-30 03:11:26 -0400

    Add 12 lines last edited by Lingjie Mei in worldgen/assets/corals/__init__.py
    Commit made automatically to show authorship. This version of the code is not usable.

---
+++ worldgen/assets/corals/__init__.py
@@ -0,0 +1,12 @@
+from .diff_growth import DiffGrowthBaseCoralFactory, TableBaseCoralFactory, LeatherBaseCoralFactory
+from .generate import CoralFactory, LeatherCoralFactory, TableCoralFactory, CauliflowerCoralFactory, \
+    BrainCoralFactory, HoneycombCoralFactory, BushCoralFactory, TwigCoralFactory, TubeCoralFactory, \
+    FanCoralFactory, ElkhornCoralFactory, StarCoralFactory
+from .laplacian import CauliflowerBaseCoralFactory
+from .elkhorn import ElkhornBaseCoralFactory
+from .reaction_diffusion import BrainBaseCoralFactory, HoneycombBaseCoralFactory, \
+    ReactionDiffusionBaseCoralFactory
+from .tree import BushBaseCoralFactory, TreeBaseCoralFactory, TwigBaseCoralFactory
+from .tube import TubeBaseCoralFactory
+from .fan import FanBaseCoralFactory
+from .star import StarBaseCoralFactory
