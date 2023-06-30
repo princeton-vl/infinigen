823b2b3cb5ffe1f4ab7ff80ec62ea050b60853cd
Lingjie Mei <lm5483@princeton.edu>
2023-06-30 03:11:26 -0400

    Add 9 lines last edited by Lingjie Mei in worldgen/assets/monocot/__init__.py
    Commit made automatically to show authorship. This version of the code is not usable.

---
+++ worldgen/assets/monocot/__init__.py
@@ -0,0 +1,9 @@
+from .agave import AgaveMonocotFactory
+from .grasses import GrassesMonocotFactory, WheatMonocotFactory, WheatEarMonocotFactory, MaizeMonocotFactory, \
+    ReedMonocotFactory
+from .tussock import TussockMonocotFactory
+from .pinecone import PineconeFactory
+from .generate import MonocotFactory
+from .banana import BananaMonocotFactory, TaroMonocotFactory
+from .veratrum import VeratrumMonocotFactory
+from .kelp import KelpMonocotFactory
