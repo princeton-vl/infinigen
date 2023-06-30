729397bdc13ef52a0c79feb62084d95d48e29856
Zeyu Ma <zeyum@princeton.edu>
2023-06-30 03:11:42 -0400

    Add 36 lines last edited by Zeyu Ma in worldgen/terrain/source/cuda/elements/mountains.cu
    Commit made automatically to show authorship. This version of the code is not usable.

---
+++ worldgen/terrain/source/cuda/elements/mountains.cu
@@ -0,0 +1,36 @@
+__global__ void mountains_kernel(
+    size_t size,
+    float3_nonbuiltin *position,
+    float *sdfs,
+    int *i_params,
+    float *f_params
+) {
+    size_t idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx < size) {
+        mountains(position[idx], sdfs + idx, i_params, f_params);
+    }
+}
+
+
+
+extern "C" {
+
+    void call(
+        size_t size,
+        float3_nonbuiltin *positions,
+        float *sdfs
+    ) {
+        using namespace data;
+        float3_nonbuiltin *d_positions;
+        cudaMalloc((void **)&d_positions, size * sizeof(float3_nonbuiltin));
+        cudaMemcpy(d_positions, positions, size * sizeof(float3_nonbuiltin), cudaMemcpyHostToDevice);
+        float *d_sdfs;
+        cudaMalloc((void **)&d_sdfs, size * sizeof(float));
+        mountains_kernel<<<ceil(size / 256.0), 256>>>(size, d_positions, d_sdfs, d_i_params, d_f_params);
+        cudaMemcpy(sdfs, d_sdfs, size * sizeof(float), cudaMemcpyDeviceToHost);
+        cudaFree(d_positions);
+        cudaFree(d_sdfs);
+    }
+
+
+}
