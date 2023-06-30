75760ac1a896d966cc42702ffd9069ff24d47eb2
Lingjie Mei <lm5483@princeton.edu>
2023-06-30 03:11:09 -0400

    Add 10 lines last edited by Lingjie Mei in worldgen/surfaces/scatters/seashells.py
    Commit made automatically to show authorship. This version of the code is not usable.

--- worldgen/surfaces/scatters/seashells.py
+++ worldgen/surfaces/scatters/seashells.py
@@ -1,15 +1,23 @@
+import numpy as np
 from numpy.random import uniform as U
+
+from assets.mollusk import MolluskFactory
 from placement.factory import AssetFactory, make_asset_collection
 from placement.instance_scatter import scatter_instances
 from surfaces.scatters.chopped_trees import approx_settle_transform
+
 from util.random import random_general as rg
+
 def apply(obj, density=('uniform', 0.2, 1.7), n=10, selection=None):
+    n_species = np.random.randint(4, 6)
+    factories = list(MolluskFactory(np.random.randint(1e5)) for _ in range(n_species))
     mollusk = make_asset_collection(
         factories, name='mollusk', verbose=True,
         weights=np.random.uniform(0.5, 1, len(factories)), n=n)
     
     #for o in mollusk.objects:
     #    approx_settle_transform(o, samples=30)
+
     scatter_obj = scatter_instances(
         base_obj=obj, collection=mollusk,
         vol_density=rg(density),
@@ -17,3 +25,5 @@ def apply(obj, density=('uniform', 0.2, 1.7), n=10, selection=None):
         selection=selection, taper_density=True,
         ground_offset=lambda nw: nw.uniform(0.07, .2)
     )
+
+    return scatter_obj, mollusk
