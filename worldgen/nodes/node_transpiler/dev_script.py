8fb40f4a83581826bc0cb292d598d5ca39dbb210
Alexander Raistrick <araistrick@princeton.edu>
2023-06-30 03:11:10 -0400

    Add 70 lines last edited by Alexander Raistrick in worldgen/nodes/node_transpiler/dev_script.py
    Commit made automatically to show authorship. This version of the code is not usable.

---
+++ worldgen/nodes/node_transpiler/dev_script.py
@@ -0,0 +1,70 @@
+'''
+1. Create a blender file at worldgen/dev_scene.blend
+2. Click the "Scripting" tab
+3. Copy this script into a new script
+4. Set the 'mode' to one of the following options:
+   - mode='print' will print the result script to your terminal
+   - mode='make_script' will create a new script in your blender UI, which you can open
+        and run to apply the node code to an object of your choice
+   - mode='write_file' will write the script to a new file called 'worldgen/generated_surface_script.py.
+        Make sure not to rename / move the script before committing it to git.     
+5. Select an object which has some materials and/or geometry nodes on it
+6. Click the play button at the top of this script to run it!
+7. You should see one python function printed for each material/geonodes on your object
+'''
+
+import os
+import sys
+import importlib
+from pathlib import Path
+import pdb
+
+import bpy
+import mathutils
+
+sys.path.append(os.getcwd())
+
+from nodes.node_transpiler import transpiler
+
+mode = 'write_file'
+target = 'object'
+
+dependencies = [
+    'assets.creatures.nodegroups.attach',
+    'assets.creatures.nodegroups.curve',
+    'assets.creatures.nodegroups.geometry',
+    'assets.creatures.nodegroups.hair',
+    'assets.creatures.nodegroups.math',
+    'assets.creatures.nodegroups.shader',
+]
+
+if target == 'object':
+    res = transpiler.transpile_object(bpy.context.active_object, dependencies)
+elif target == 'world':
+    res = transpiler.transpile_world(dependencies)
+else:
+    raise ValueError(f'Unrecognized {target=}')
+
+if mode == 'print':
+    print('START')
+    print('\n')
+    print(res)
+    print('END')
+elif mode == 'make_script':
+    res_debug = (
+        "import bpy\n" +
+        res +
+        "\napply(bpy.context.active_object)"
+    )
+    script = bpy.data.texts.new('generated_surface_script')
+    script.from_string(res_debug)
+elif mode == 'write_file':
+    
+    filename = 'generated_surface_script.py'
+    print(f'Writing generated script to {filename}')
+    with Path(filename).open('w') as f:
+        f.write(res)
+else:
+    raise ValueError(f'Unrecognized {mode=}')
+
+
