e1de6bc9d0b478cab950dd3d29f90ac39aa063a2
Alexander Raistrick <araistrick@princeton.edu>
2023-06-30 03:11:07 -0400

    Add 9 lines last edited by Alexander Raistrick in worldgen/surfaces/templates/twocolorz.py
    Commit made automatically to show authorship. This version of the code is not usable.

--- worldgen/surfaces/templates/twocolorz.py
+++ worldgen/surfaces/templates/twocolorz.py
@@ -1,15 +1,20 @@
 import os, sys
 import numpy as np
 import math as ma
+from surfaces.surface_utils import clip, sample_range, sample_ratio, sample_color, geo_voronoi_noise
 import bpy
 import mathutils
 from numpy.random import uniform, normal
+from nodes.node_wrangler import Nodes, NodeWrangler
 from surfaces import surface
+
+def shader_twocolorz(nw, rand=True, **input_kwargs):
 
     texture_coordinate = nw.new_node(Nodes.TextureCoord)
     
     mapping = nw.new_node(Nodes.Mapping,
         input_kwargs={'Vector': texture_coordinate.outputs["Generated"]})
+    if rand:
         for i in range(2):
             # do not change Z
             mapping.inputs['Location'].default_value[i] = sample_range(-2, 2)
@@ -19,6 +24,7 @@ from surfaces import surface
     
     noise_texture = nw.new_node(Nodes.NoiseTexture,
         input_kwargs={'Vector': mapping, 'Scale': 10.0, 'Detail': 3.0, 'Distortion': 0.5})
+    if rand:
         for k in ['Scale', 'Detail', 'Distortion', 'Roughness']:
             noise_texture.inputs[k].default_value = sample_ratio(noise_texture.inputs[k].default_value, 1/3, 3)
 
@@ -32,6 +38,7 @@ from surfaces import surface
     colorramp.color_ramp.elements[0].color = (0.2059, 1.0, 0.1039, 1.0)
     colorramp.color_ramp.elements[1].position = 0.5364
     colorramp.color_ramp.elements[1].color = (1.0, 0.0047, 0.2941, 1.0)
+    if rand:
         pos_max = [0.4, 0.8]
         colorramp.color_ramp.elements[0].position = sample_range(0, pos_max[0])
         _min = (pos_max[1] - colorramp.color_ramp.elements[0].position) / 3 + colorramp.color_ramp.elements[0].position
@@ -55,4 +62,6 @@ from surfaces import surface
     material_output = nw.new_node(Nodes.MaterialOutput,
         input_kwargs={'Surface': principled_bsdf})
 
+def apply(obj, geo_kwargs=None, shader_kwargs=None, **kwargs):
+    surface.add_geomod(obj, geo_voronoi_noise, apply=False, input_kwargs=geo_kwargs, attributes=['offset'])
     surface.add_material(obj, shader_twocolorz, reuse=False, input_kwargs=shader_kwargs)
