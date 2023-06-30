5fa14fd2d9b71e1aca6b25b7688ddadf71bb5177
Alexander Raistrick <araistrick@princeton.edu>
2023-06-30 03:11:03 -0400

    Add 93 lines last edited by Alexander Raistrick in worldgen/surfaces/templates/chitin.py
    Commit made automatically to show authorship. This version of the code is not usable.

--- worldgen/surfaces/templates/chitin.py
+++ worldgen/surfaces/templates/chitin.py
@@ -2,9 +2,55 @@ import os, sys
 import numpy as np
 import math as ma
 from surfaces.surface_utils import clip, sample_range, sample_ratio, sample_color, geo_voronoi_noise
+import bpy
+import mathutils
+from numpy.random import uniform, normal, randint
+from nodes.node_wrangler import Nodes, NodeWrangler
+from nodes import node_utils
+from nodes.color import color_category, hsv2rgba
+from surfaces import surface
+
 def shader_chitin(nw: NodeWrangler, rand=True, **input_kwargs):
+    # Code generated using version 2.4.3 of the node_transpiler
+
+    geometry = nw.new_node('ShaderNodeNewGeometry')
+    
+    colorramp_1 = nw.new_node(Nodes.ColorRamp,
+        input_kwargs={'Fac': geometry.outputs["Pointiness"]})
+    colorramp_1.color_ramp.elements.new(0)
+    colorramp_1.color_ramp.elements[0].position = 0.4091
+    colorramp_1.color_ramp.elements[0].color = (0.0, 0.0, 0.0, 1.0)
+    colorramp_1.color_ramp.elements[1].position = 0.4455
+    colorramp_1.color_ramp.elements[1].color = (1.0, 1.0, 1.0, 1.0)
     colorramp_1.color_ramp.elements[2].position = 0.5127
+    colorramp_1.color_ramp.elements[2].color = (0.0, 0.0, 0.0, 1.0)
+    
+    colorramp_10 = nw.new_node(Nodes.ColorRamp,
+        input_kwargs={'Fac': colorramp_1.outputs["Color"]})
+    colorramp_10.color_ramp.elements[0].position = 0.0
+    colorramp_10.color_ramp.elements[0].color = (0.0, 0.0, 0.0, 1.0)
+    colorramp_10.color_ramp.elements[1].position = 0.2273
+    colorramp_10.color_ramp.elements[1].color = (1.0, 1.0, 1.0, 1.0)
+    
+    colorramp_4 = nw.new_node(Nodes.ColorRamp,
+        input_kwargs={'Fac': geometry.outputs["Pointiness"]})
+    colorramp_4.color_ramp.elements[0].position = 0.4909
+    colorramp_4.color_ramp.elements[0].color = (0.0, 0.0, 0.0, 1.0)
+    colorramp_4.color_ramp.elements[1].position = 0.6773
+    colorramp_4.color_ramp.elements[1].color = (1.0, 1.0, 1.0, 1.0)
+    
+    multiply = nw.new_node(Nodes.Math,
+        input_kwargs={0: colorramp_10.outputs["Color"], 1: colorramp_4.outputs["Color"]},
+        attrs={'operation': 'MULTIPLY'})
+    
+    colorramp_3 = nw.new_node(Nodes.ColorRamp,
+        input_kwargs={'Fac': multiply})
+    colorramp_3.color_ramp.interpolation = "EASE"
+    colorramp_3.color_ramp.elements[0].position = 0.0
+    colorramp_3.color_ramp.elements[0].color = (0.0, 0.0, 0.0, 1.0)
     colorramp_3.color_ramp.elements[1].position = 0.0864
+    colorramp_3.color_ramp.elements[1].color = (1.0, 1.0, 1.0, 1.0)
+    
     texture_coordinate_1 = nw.new_node(Nodes.TextureCoord)
     
     separate_xyz_3 = nw.new_node(Nodes.SeparateXYZ,
@@ -52,16 +98,55 @@ def shader_chitin(nw: NodeWrangler, rand=True, **input_kwargs):
         input_kwargs={0: multiply_2, 1: invert_1},
         attrs={'operation': 'MULTIPLY'})
     
+    texture_coordinate = nw.new_node(Nodes.TextureCoord)
+    
+    separate_xyz_2 = nw.new_node(Nodes.SeparateXYZ,
+        input_kwargs={'Vector': texture_coordinate.outputs["Object"]})
+    
+    sign = nw.new_node(Nodes.Math,
+        input_kwargs={0: separate_xyz_2.outputs["X"]},
+        attrs={'operation': 'SIGN'})
+    
+    mapping_1 = nw.new_node(Nodes.Mapping,
         input_kwargs={'Vector': texture_coordinate.outputs["Object"], 'Rotation': (0.0, 0.0, -0.7854), 'Scale': (1.0, 1.0, 0.0)})
+    
+    separate_xyz_1 = nw.new_node(Nodes.SeparateXYZ,
+        input_kwargs={'Vector': mapping_1})
+    
+    mapping = nw.new_node(Nodes.Mapping,
         input_kwargs={'Vector': texture_coordinate.outputs["Object"], 'Rotation': (0.0, 0.0, 0.7854), 'Scale': (1.0, 10.0, 0.0)})
+    
+    separate_xyz = nw.new_node(Nodes.SeparateXYZ,
+        input_kwargs={'Vector': mapping})
+    
+    mix_3 = nw.new_node(Nodes.MixRGB,
+        input_kwargs={'Fac': sign, 'Color1': separate_xyz_1.outputs["X"], 'Color2': separate_xyz.outputs["X"]})
+    
+    noise_texture = nw.new_node(Nodes.NoiseTexture,
+        input_kwargs={'Scale': 10.0, 'Detail': 10.0})
+    
+    mix = nw.new_node(Nodes.MixRGB,
+        input_kwargs={'Fac': 0.2, 'Color1': mix_3, 'Color2': noise_texture.outputs["Fac"]})
+    
+    noise_texture_1 = nw.new_node(Nodes.NoiseTexture,
         input_kwargs={'Vector': mix, 'W': 1.4, 'Scale': 100.0, 'Detail': 10.0, 'Roughness': 0.0},
+        attrs={'noise_dimensions': '4D'})
     if rand:
         noise_texture_1.inputs['W'].default_value = sample_range(-2,2)
+    
+    mix_2 = nw.new_node(Nodes.MixRGB,
         input_kwargs={'Fac': multiply_3, 'Color1': (0.0, 0.0, 0.0, 1.0), 'Color2': noise_texture_1.outputs["Fac"]})
+    
+    colorramp_2 = nw.new_node(Nodes.ColorRamp,
+        input_kwargs={'Fac': mix_2})
+    colorramp_2.color_ramp.elements[0].position = 0.0
     colorramp_2.color_ramp.elements[0].color = (0.0068, 0.0, 0.0005, 1.0)
     colorramp_2.color_ramp.elements[1].position = 0.9955
     colorramp_2.color_ramp.elements[1].color = (0.1347, 0.0156, 0.0115, 1.0)
     if rand:
+        colorramp_2.color_ramp.elements[1].color = hsv2rgba((np.mod(normal(0.2, 0.4), 1), uniform(0, 1), uniform(0.05, 0.5)))
+        #for i in range(3):
+        #    colorramp_2.color_ramp.elements[1].color[i] /= 7
 
     invert = nw.new_node('ShaderNodeInvert',
         input_kwargs={'Color': multiply_2})
@@ -72,7 +157,14 @@ def shader_chitin(nw: NodeWrangler, rand=True, **input_kwargs):
     colorramp_11.color_ramp.elements[0].color = (0.0, 0.0, 0.0, 1.0)
     colorramp_11.color_ramp.elements[1].position = 1.0
     colorramp_11.color_ramp.elements[1].color = (0.5103, 0.5103, 0.5103, 1.0)
+    
+    principled_bsdf = nw.new_node(Nodes.PrincipledBSDF,
         input_kwargs={'Base Color': colorramp_2.outputs["Color"], 'Metallic': 0.7, 'Roughness': colorramp_11.outputs["Color"]},
+        attrs={'subsurface_method': 'BURLEY'})
+    
+    material_output = nw.new_node(Nodes.MaterialOutput,
+        input_kwargs={'Surface': principled_bsdf})
+
 def geometry_chitin(nw: NodeWrangler, rand=True, **input_kwargs):
     # Code generated using version 2.4.3 of the node_transpiler
 
@@ -123,3 +215,4 @@ if __name__ == "__main__":
         bpy.ops.wm.save_as_mainfile(filepath=fn)
         #bpy.context.scene.render.filepath = os.path.join('surfaces/surface_thumbnails', 'bone%d.jpg'%(i))
         #bpy.context.scene.render.image_settings.file_format='JPEG'
+        #bpy.ops.render.render(write_still=True)
