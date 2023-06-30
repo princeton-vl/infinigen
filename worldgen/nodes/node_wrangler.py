a42f50b78412a463e7c405a98fa31d7b82945f66
Karhan Kayan <karhan@princeton.edu>
2023-06-30 03:11:10 -0400

    Add 18 lines last edited by Karhan Kayan in worldgen/nodes/node_wrangler.py
    Commit made automatically to show authorship. This version of the code is not usable.

--- worldgen/nodes/node_wrangler.py
+++ worldgen/nodes/node_wrangler.py
@@ -20,6 +20,24 @@ class NodeMisuseWarning(UserWarning):
     pass
 
 
+# This is for Blender 3.3 because of the nodetree change
+def geometry_node_group_empty_new():
+    group = bpy.data.node_groups.new("Geometry Nodes", 'GeometryNodeTree')
+    group.inputs.new('NodeSocketGeometry', "Geometry")
+    group.outputs.new('NodeSocketGeometry', "Geometry")
+    input_node = group.nodes.new('NodeGroupInput')
+    output_node = group.nodes.new('NodeGroupOutput')
+    output_node.is_active_output = True
+
+    input_node.select = False
+    output_node.select = False
+
+    input_node.location.x = -200 - input_node.width
+    output_node.location.x = 200
+
+    group.links.new(output_node.inputs[0], input_node.outputs[0])
+
+    return group
 
 
 def warn_with_traceback(message, category, filename, lineno, file=None, line=None):
