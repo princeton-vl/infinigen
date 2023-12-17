e23073612e4d953d0c3a6f3639262e566a69ab9a
Alexander Raistrick <araistrick@princeton.edu>
2023-12-17 13:45:58 -0500

    Fix hello world crash

--- infinigen/__init__.py
+++ infinigen/__init__.py
@@ -1,3 +1,3 @@
 import logging
 
-__version__ = "1.2.0.1"
+__version__ = "1.2.0.2"
--- infinigen/terrain/utils/mesh.py
+++ infinigen/terrain/utils/mesh.py
@@ -180,10 +180,11 @@ class Mesh:
         if material is not None:
             new_object.data.materials.append(material)
 
+        butil.put_in_collection(bpy.data.objects[name], butil.get_collection('terrain'))
+
         with butil.SelectObjects(new_object):
             bpy.ops.object.shade_flat()
 
-        butil.put_in_collection(bpy.data.objects[name], butil.get_collection('terrain'))
         return new_object
     
     @property
