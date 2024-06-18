632f2ffd065c16143de6471cfea3c6144dfda2d8
Alexander Raistrick <araistrick@princeton.edu>
2024-06-17 17:24:18 -0700

    Add 88 lines to infinigen/core/constraints/example_solver/geometry/stability.py. Contributed as part of Infinigen-Indoors by Alexander Raistrick.

--- infinigen/core/constraints/example_solver/geometry/stability.py
+++ infinigen/core/constraints/example_solver/geometry/stability.py
@@ -19,11 +19,19 @@ from shapely import MultiPolygon
 import bmesh
 
 import matplotlib.pyplot as plt
+import gin
 # import fcl
 
 # from infinigen.core.util import blender as butil
+from infinigen.core.constraints.example_solver.geometry import planes as planes
 from infinigen.core.util import blender as butil
 from mathutils import Vector, Quaternion
+
+from infinigen.core.constraints.example_solver import state_def
+from infinigen.core.constraints import constraint_language as cl, reasoning as r
+
+from infinigen.core.constraints.constraint_language import util as iu
+
 import logging
 logger = logging.getLogger(__name__)
 
@@ -73,35 +81,81 @@ def project_vector(vector, origin, normal):
     transformed_vector = transformed[1] - transformed[0]
     return transformed_vector
 
+@gin.configurable
+def stable_against(
+    state: state_def.State, 
+    obj_name: str, 
+    relation_state: state_def.RelationState, 
+    visualize=False,
+    allow_overhangs=False
+):
     """
     check paralell, close to, and not overhanging. 
     """
 
+    relation = relation_state.relation
+    assert isinstance(relation, cl.StableAgainst)
+
     logger.debug(f'stable against {obj_name=} {relation_state=}')
+    a_blender_obj = state.objs[obj_name].obj
+    b_blender_obj = state.objs[relation_state.target_name].obj
+    sa = state.objs[obj_name]
+    sb = state.objs[relation_state.target_name]
+
     pa, pb = state.planes.get_rel_state_planes(state, obj_name, relation_state)
 
     poly_a = state.planes.planerep_to_poly(pa)
     poly_b = state.planes.planerep_to_poly(pb)
 
+    normal_a = iu.global_polygon_normal(a_blender_obj, poly_a)
+    normal_b = iu.global_polygon_normal(b_blender_obj, poly_b)
+    dot = np.array(normal_a).dot(normal_b)
     if not (np.isclose(np.abs(dot), 1, atol=1e-2) or np.isclose(dot, -1, atol=1e-2)):
         logger.debug(f'stable against failed, not parallel {dot=}')
         return False
+    
+    origin_b = iu.global_vertex_coordinates(b_blender_obj, b_blender_obj.data.vertices[poly_b.vertices[0]])
+
+    scene = state.trimesh_scene
+    a_trimesh = iu.meshes_from_names(scene, sa.obj.name)[0]
+    b_trimesh = iu.meshes_from_names(scene, sb.obj.name)[0]
 
 
+    mask = tagging.tagged_face_mask(sb.obj, relation.parent_tags)
     mask = state.planes.tagged_plane_mask(sb.obj, mask, pb)
+    assert mask.any()
     # Project mesh A onto the plane of mesh B
+    projected_a = trimesh.path.polygons.projected(a_trimesh, normal_b, origin_b)
+    projected_b = trimesh.path.polygons.projected(b_trimesh_mask, normal_b, origin_b)
+    logger.debug(f'stable_against projecting along {normal_b} for parent_tags {relation.parent_tags}')
 
 
     if projected_a is None or projected_b is None:
         raise ValueError(f'Invalid {projected_a=} {projected_b=}')
 
+    if allow_overhangs:
+        res = projected_a.overlaps(projected_b)
+    elif relation.check_z:
         res = projected_a.within(projected_b.buffer(1e-2))
+    else:
         z_proj = project_vector(np.array([0, 0, 1]), origin_b, normal_b)
         projected_a_rotated, projected_b_rotated = project_and_align_z_with_x([projected_a, projected_b], z_proj)
         res = is_vertically_contained(projected_a_rotated, projected_b_rotated)
+
+    if visualize:
+        fig, ax = plt.subplots()
+        iu.plot_geometry(ax, projected_a, 'blue')
+        iu.plot_geometry(ax, projected_b, 'green')
+        plt.title(f'{obj_name} stable against {relation_state.target_name}? {res=}')
+        plt.show()
+
+    logger.debug(f'stable_against {res=}')
+    if not res:
         return False
     
     for vertex in poly_a.vertices:
+        vertex_global = iu.global_vertex_coordinates(a_blender_obj, a_blender_obj.data.vertices[vertex])
+        distance = iu.distance_to_plane(vertex_global, origin_b, normal_b)
         if not np.isclose(distance, relation_state.relation.margin, atol=1e-2):
             logger.debug(f'stable against failed, not close to {distance=}')
             return False
@@ -122,10 +176,18 @@ def snap_against(scene, a, b, a_plane, b_plane, margin = 0):
     a_poly = a_obj.data.polygons[a_poly_index]
     b_poly_index = b_plane[1]
     b_poly = b_obj.data.polygons[b_poly_index]
+    plane_point_a = iu.global_vertex_coordinates(a_obj, a_obj.data.vertices[a_poly.vertices[0]])
+    plane_normal_a = iu.global_polygon_normal(a_obj, a_poly)
+    plane_point_b = iu.global_vertex_coordinates(b_obj, b_obj.data.vertices[b_poly.vertices[0]])
+    plane_normal_b = iu.global_polygon_normal(b_obj, b_poly)
     plane_normal_b = -plane_normal_b
 
 
     
+    norm_mag_a = np.linalg.norm(plane_normal_a)
+    norm_mag_b = np.linalg.norm(plane_normal_b)
+    assert np.isclose(norm_mag_a, 1), norm_mag_a
+    assert np.isclose(norm_mag_b, 1), norm_mag_b
 
     rotation_axis = np.cross(plane_normal_a, plane_normal_b)
     if not np.isclose(np.linalg.norm(rotation_axis),0, atol = 1e-05): 
@@ -133,15 +195,26 @@ def snap_against(scene, a, b, a_plane, b_plane, margin = 0):
     else:
         rotation_axis = np.array([0,0,1])
 
+    dot = plane_normal_a.dot(plane_normal_b)
+    rotation_angle = np.arccos(np.clip(dot, -1, 1))
+    if np.isnan(rotation_angle):
+        raise ValueError(f'Invalid {rotation_angle=}')
+    iu.rotate(scene, a, rotation_axis, rotation_angle)
+
 
 
     a_obj = bpy.data.objects[a]
     a_poly = a_obj.data.polygons[a_poly_index]
     # Recalculate vertex_a and normal_a after rotation
+    plane_point_a = iu.global_vertex_coordinates(a_obj, a_obj.data.vertices[a_poly.vertices[0]])
+    plane_normal_a = iu.global_polygon_normal(a_obj, a_poly)
     
     distance = (plane_point_a - plane_point_b).dot(plane_normal_b)
 
     # Move object a by the average distance minus the margin in the direction of the plane normal of b
+    translation = -(distance + margin) * plane_normal_b.normalized()
+    iu.translate(scene, a, translation)
+
 
 
 
@@ -149,6 +222,10 @@ def random_sample_point(state: state_def.State, obj: bpy.types.Object, face_mask
     """
     Given a plane, return a random point on the plane.
     """
+
+    if obj.type != 'MESH':
+        raise ValueError(f'Unexpected {obj.type=}')
+
     plane_mask = state.planes.tagged_plane_mask(obj, face_mask, plane)
     if not np.any(plane_mask):
         logging.warning(
@@ -157,6 +234,7 @@ def random_sample_point(state: state_def.State, obj: bpy.types.Object, face_mask
 
     # Create a bmesh from the object mesh
     bm = bmesh.new()
+    bm.from_mesh(obj.data)
     bm.faces.ensure_lookup_table()  
 
     faces = [bm.faces[i] for i in np.where(plane_mask)[0]]
@@ -189,8 +267,10 @@ def move_obj_random_pt(state: state_def.State, a, b, face_mask: np.ndarray, plan
     move a to a random point on b
     """
     scene = state.trimesh_scene
+    b_obj = iu.blender_objs_from_names(b)[0]
 
     random_point_global = random_sample_point(state, b_obj, face_mask, plane)
+    iu.set_location(scene, a, random_point_global)
 
 
 # def place_randomly(scene, a, b, visualize = False):
@@ -226,10 +306,15 @@ def supported_by(scene, a, b, visualize = False):
     if isinstance(a, str):
         a = [a]
 
+    a_meshes = iu.blender_objs_from_names(a)
+    a_trimeshes = iu.meshes_from_names(scene, a)
+    b_mesh = iu.blender_objs_from_names(b)[0]
+    b_trimesh = iu.meshes_from_names(scene, b)[0]
 
     if visualize:
         fig, ax = plt.subplots()
         ax.set_aspect('equal', 'box')
+        b_poly = iu.project_to_xy_poly(b_trimesh)
         if isinstance(b_poly, Polygon):
             x, y = b_poly.exterior.xy
             ax.fill(x, y, alpha=0.5, fc='red', ec='black', label='Polygon b')
@@ -245,6 +330,9 @@ def supported_by(scene, a, b, visualize = False):
         butil.modify_mesh(
             cloned_a, "BOOLEAN", apply=True, operation="INTERSECT", object=b_mesh
         )
+        iu.preprocess_obj(cloned_a)
+        intersection = iu.to_trimesh(cloned_a)
+        intersection_poly = iu.project_to_xy_poly(intersection)
         intersection_convex = intersection_poly.convex_hull
         com_projected = a_trimesh.centroid[:2]
         if visualize:
