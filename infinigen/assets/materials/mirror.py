87539df7692a4956877325fb053c2f9d48f5731e
Lingjie Mei <lm5483@princeton.edu>
2024-06-17 17:24:22 -0700

    Add 18 lines to infinigen/assets/materials/mirror.py. Contributed as part of Infinigen-Indoors by Lingjie Mei.

---
+++ infinigen/assets/materials/mirror.py
@@ -0,0 +1,18 @@
+# Copyright (c) Princeton University.
+# This source code is licensed under the BSD 3-Clause license found in the LICENSE file in the root directory of this source tree.
+
+# Authors: Lingjie Mei
+from infinigen.assets.materials import common
+from infinigen.core.nodes import NodeWrangler, Nodes
+
+
+def shader_mirror(nw: NodeWrangler,**kwargs):
+    glossy_bsdf = nw.new_node('ShaderNodeBsdfGlossy',
+                              input_kwargs={'Color': (1.0, 1.0, 1.0, 1.0), 'Roughness': 0,
+                              })
+
+    nw.new_node(Nodes.MaterialOutput, input_kwargs={'Surface': glossy_bsdf})
+
+
+def apply(obj, selection=None, **kwargs):
+    common.apply(obj, shader_mirror, selection, **kwargs)
