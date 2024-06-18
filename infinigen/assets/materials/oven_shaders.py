70a7347e0116f9b9e42bd52c5dcf1d8436f5376a
Hongyu Wen <hongyu.wen@princeton.edu>
2024-06-17 17:24:22 -0700

    Add 5 lines to infinigen/assets/materials/oven_shaders.py. Contributed as part of Infinigen-Indoors by Hongyu Wen.

--- infinigen/assets/materials/oven_shaders.py
+++ infinigen/assets/materials/oven_shaders.py
@@ -1,3 +1,8 @@
+# Copyright (c) Princeton University.
+# This source code is licensed under the BSD 3-Clause license found in the LICENSE file in the root directory of this source tree.
+
+# Authors: Hongyu Wen
+
 from infinigen.core.nodes.node_wrangler import Nodes, NodeWrangler
 
 
