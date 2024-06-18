d4b97878d3ab2e04a191c12ff0aa95851a3110b0
Alexander Raistrick <araistrick@princeton.edu>
2024-06-17 17:24:34 -0700

    Add 2 lines to infinigen/assets/seating/__init__.py. Contributed as part of Infinigen-Indoors by Alexander Raistrick.

--- infinigen/assets/seating/__init__.py
+++ infinigen/assets/seating/__init__.py
@@ -2,6 +2,8 @@
 # This source code is licensed under the BSD 3-Clause license found in the LICENSE file in the root directory of this source tree.
 
 # Authors: Lingjie Mei
+
+from .sofa import SofaFactory, ArmChairFactory
 from .bedframe import BedFrameFactory
 from .pillow import PillowFactory
 from .mattress import MattressFactory
