ea2aaff5e119f7295497733c8f47e33a76f47817
Lingjie Mei <lm5483@princeton.edu>
2024-06-17 17:24:32 -0700

    Add 18 lines to infinigen/assets/elements/staircases/__init__.py. Contributed as part of Infinigen-Indoors by Lingjie Mei.

---
+++ infinigen/assets/elements/staircases/__init__.py
@@ -0,0 +1,18 @@
+# Copyright (c) Princeton University.
+# This source code is licensed under the BSD 3-Clause license found in the LICENSE file in the root directory of this source tree.
+
+# Authors: Lingjie Mei
+import numpy as np
+from .curved import CurvedStaircaseFactory
+from .spiral import SpiralStaircaseFactory
+from .straight import StraightStaircaseFactory
+from .l_shaped import LShapedStaircaseFactory
+from .u_shaped import UShapedStaircaseFactory
+from .cantilever import CantileverStaircaseFactory
+
+
+def random_staircase_factory():
+    door_factories = [StraightStaircaseFactory, LShapedStaircaseFactory, UShapedStaircaseFactory,
+        SpiralStaircaseFactory, CurvedStaircaseFactory, CantileverStaircaseFactory]
+    door_probs = np.array([2, 2, 2, .5, 2, 2])
+    return np.random.choice(door_factories, p=door_probs / door_probs.sum())
