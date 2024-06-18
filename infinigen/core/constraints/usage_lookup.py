7e14fe61ffae2622d489da0e139f85959e8c43fa
Alexander Raistrick <araistrick@princeton.edu>
2024-06-17 17:24:14 -0700

    Add 45 lines to infinigen/core/constraints/usage_lookup.py. Contributed as part of Infinigen-Indoors by Alexander Raistrick.

---
+++ infinigen/core/constraints/usage_lookup.py
@@ -0,0 +1,45 @@
+# Copyright (c) Princeton University.
+# This source code is licensed under the BSD 3-Clause license found in the LICENSE file in the root directory
+# of this source tree.
+
+# Authors: Alexander Raistrick
+
+from collections import defaultdict
+
+_factory_lookup: dict[type, set[t.Tag]] = None
+_tag_lookup: dict[t.Tag, set[type]] = None
+
+def initialize_from_dict(d):
+
+    global _factory_lookup, _tag_lookup
+    _factory_lookup = defaultdict(set)
+    _tag_lookup = defaultdict(set)
+
+    for tag, fac_list in d.items():
+        _tag_lookup[tag] = set()
+        for fac in fac_list:
+            _factory_lookup[fac].add(tag)
+            _tag_lookup[tag].add(fac)
+
+    return _factory_lookup[fac].union({t.FromGenerator(fac)})
+
+    if not isinstance(tags, set):
+        tags = [tags]
+    else:
+        tags = list(tags)
+
+    res = _tag_lookup[tags[0]]
+    for t in tags[1:]:
+        res.intersection_update(_tag_lookup[t])
+    return res
+
+def all_usage_tags():
+    return _tag_lookup.keys()
+
+def all_factories():
+    return _factory_lookup.keys()
+
+def has_usage(fac, tag):
+    assert fac in _factory_lookup.keys(), fac
+    assert tag in _tag_lookup.keys(), tag
+    return tag in _factory_lookup[fac]
