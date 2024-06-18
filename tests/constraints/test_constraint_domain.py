d6b84bb8c24a98efbb525b08e079383caef84b25
Alexander Raistrick <araistrick@princeton.edu>
2024-06-17 17:24:11 -0700

    Add 170 lines to tests/constraints/test_constraint_domain.py. Contributed as part of Infinigen-Indoors by Alexander Raistrick.

---
+++ tests/constraints/test_constraint_domain.py
@@ -0,0 +1,170 @@
+# Copyright (c) Princeton University.
+# This source code is licensed under the BSD 3-Clause license found in the LICENSE file in the root directory
+# of this source tree.
+
+# Authors: Alexander Raistrick
+
+import logging
+
+from infinigen.core.constraints import (
+    reasoning as r,
+    constraint_language as cl
+)
+
+from infinigen_examples.util import constraint_util as cu
+
+def test_domain_obj():
+    
+     
+    assert sofas.implies(furniture)
+    assert not furniture.implies(sofas)
+
+    assert not furniture.implies(furniture_in_livingroom)
+    assert furniture_in_livingroom.implies(furniture)
+
+    assert not furniture_in_livingroom.implies(furniture_in_bathroom)
+    assert not furniture_in_bathroom.implies(furniture_in_livingroom)
+
+def test_domain_implies_complex():
+
+    against_wall = cl.StableAgainst(
+        margin=0
+    )
+
+    d = r.Domain(
+        relations=[
+            (
+                against_wall,   
+            )
+        ]
+    )
+
+    assert d.implies(d)
+    assert not r.Domain(set(), d.relations).implies(d)
+    assert d.implies(r.Domain(set(), d.relations))
+    
+    # "storage related any way to any thing" is less specific
+    generalize_relation = r.Domain(
+        relations=[(cl.AnyRelation(), r.Domain())])
+    assert d.implies(generalize_relation)
+    assert not generalize_relation.implies(d)
+
+    # "storage related any way but specifically to bedroom" 
+    #   is both more and less specific so not a subset either way
+    different_relation = r.Domain(
+    )
+    assert not d.implies(different_relation)
+    assert not different_relation.implies(d)
+
+    # "storage against a bedroom wall" is more specific
+    against_bedroom_wall = r.Domain(
+    )
+    assert against_bedroom_wall.implies(d)
+    assert not d.implies(against_bedroom_wall)
+
+def test_domain_var_substitute():
+
+
+    assert r.domain_tag_substitute(start, var, subfor) == r.Domain(
+        relations=[
+        ]
+    )
+
+    assert r.domain_tag_substitute(start2, var, subfor) == r.Domain(
+        relations=[
+        ]
+    )
+
+def test_domain_intersect_tags():
+
+    obj = cl.scene()[Semantics.Object].excludes(obj_types)
+    room = cl.scene()[Semantics.Room].excludes(obj_types)
+
+    ld = r.constraint_domain(obj)
+
+    md = r.constraint_domain(room)
+
+    assert not ld.intersects(md)
+    assert not ld.intersects(md)
+
+def test_domain_construction_complex():
+    
+    dom = r.Domain()
+    dom.add_relation(cl.AnyRelation(), r.Domain({t.Semantics.Object}, []))
+    dom.add_relation(cl.StableAgainst(), r.Domain({t.Semantics.Object, t.Variable('room')}, []))
+    dom.add_relation(-cl.AnyRelation(), r.Domain({t.Semantics.Room}, []))
+
+    assert dom.relations[0] == (cl.StableAgainst(), r.Domain({t.Semantics.Object, t.Variable('room')}, []))
+    assert dom.relations[1] == (-cl.AnyRelation(), r.Domain({t.Semantics.Room}, []))
+    assert len(dom.relations) == 2
+
+def test_domain_construction_complex_2():
+
+    rd1 = (cl.AnyRelation(), r.Domain({t.Semantics.Room, t.Semantics.DiningRoom}))
+    rd2 = (cl.StableAgainst(), r.Domain({t.Semantics.Room}))
+
+    assert r.reldom_intersects(rd1, rd2)
+
+    dom = r.Domain()
+    dom.add_relation(*rd1)
+    dom.add_relation(*rd2)
+        
+    print("DOM RESULT", dom)
+    assert dom.relations == [
+        (cl.StableAgainst(), r.Domain({t.Semantics.Room, t.Semantics.DiningRoom}))
+    ]
+
+def test_domain_satisfies():
+
+    a = r.Domain({t.Semantics.Object, -t.Semantics.Room})
+    b = r.Domain({t.Semantics.Object, t.Semantics.Room})
+    assert not b.satisfies(a)
+
+    b = r.Domain({t.Semantics.Object, -t.Semantics.Room})
+    assert b.satisfies(a)
+
+    a.add_relation(cl.StableAgainst(), r.Domain({t.Semantics.Room}))
+    assert not b.satisfies(a)
+
+    b.add_relation(cl.StableAgainst(), r.Domain({t.Semantics.Room, t.Semantics.DiningRoom}))
+    assert b.satisfies(a)
+
+    a.add_relation(-cl.AnyRelation(), r.Domain({t.Semantics.Object}))
+    assert b.satisfies(a)
+
+    b.add_relation(cl.StableAgainst(), r.Domain({t.Semantics.Object}))
+    assert not b.satisfies(a)
+
+def test_domain_satisfies_2():
+
+    res_dom = r.Domain(
+        {Semantics.Object, Semantics.Storage, -Semantics.Room}, [
+            (
+                cl.StableAgainst(
+                    {t.Subpart.Bottom, -t.Subpart.Top, -t.Subpart.Back, -t.Subpart.Front}, 
+                    {t.Subpart.Visible, t.Subpart.SupportSurface, -t.Subpart.Ceiling, -t.Subpart.Wall}
+                ), 
+                r.Domain({Semantics.DiningRoom, Semantics.Room, -Semantics.Object}, [])
+            ),
+            (
+                -cl.AnyRelation(), 
+                r.Domain({Semantics.Object, -Semantics.Room}, [])
+            )
+        ]
+    ) 
+
+    filter_dom = r.Domain(
+        {Semantics.Object, -Semantics.Room}, 
+        [
+            (
+                cl.StableAgainst(
+                    {}, 
+                    {t.Subpart.SupportSurface, t.Subpart.Visible, -t.Subpart.Ceiling, -t.Subpart.Wall}
+                ), 
+                r.Domain({Semantics.DiningRoom, Semantics.Room, -Semantics.Object}, [])
+            ),
+            (-cl.AnyRelation(), r.Domain({Semantics.Object, -Semantics.Room}, []))
+    ]
+    )
+
+    assert res_dom.satisfies(filter_dom)
