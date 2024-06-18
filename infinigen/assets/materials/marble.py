352e6ebc66af00bea2ae56a57e23d4032473e2c3
Lingjie Mei <lm5483@princeton.edu>
2024-06-17 17:24:23 -0700

    Add 9 lines to infinigen/assets/materials/marble.py. Contributed as part of Infinigen-Indoors by Lingjie Mei.

---
+++ infinigen/assets/materials/marble.py
@@ -0,0 +1,9 @@
+# Copyright (c) Princeton University.
+# This source code is licensed under the BSD 3-Clause license found in the LICENSE file in the root directory of this source tree.
+
+# Authors: Lingjie Mei
+from infinigen.assets.materials import common
+
+
+def apply(obj, selection=None, **kwargs):
+    common.apply(obj, shader_marble, selection, **kwargs)
