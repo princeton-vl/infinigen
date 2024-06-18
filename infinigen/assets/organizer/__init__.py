0dae372fc3e98193bcf8e8b55041f0d9837e808e
Beining Han <bh7023@princeton.edu>
2024-06-17 17:24:30 -0700

    Add 9 lines to infinigen/assets/organizer/__init__.py. Contributed as part of Infinigen-Indoors by Beining Han.

---
+++ infinigen/assets/organizer/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (c) Princeton University.
+# This source code is licensed under the BSD 3-Clause license found in the LICENSE file in the root directory of this source tree.
+
+# Authors: Beining Han
+
+
+from .basket import BasketBaseFactory
+from .hook import HookBaseFactory, SpatulaOnHookBaseFactory
+from .plate_rack import PlateRackBaseFactory, PlateOnRackBaseFactory
