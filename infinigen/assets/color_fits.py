474a9f049252d5240f0a23830d2da3647f2fa9bc
Stamatis Alexandropoulos <sa6924@princeton.edu>
2024-06-17 17:24:19 -0700

    Add 4 lines to infinigen/assets/color_fits.py. Contributed as part of Infinigen-Indoors by Stamatis Alexandropoulos.

--- infinigen/assets/color_fits.py
+++ infinigen/assets/color_fits.py
@@ -1,3 +1,7 @@
+# Copyright (c) Princeton University.
+# This source code is licensed under the BSD 3-Clause license found in the LICENSE file in the root directory of this source tree.
+
+
 import os
 import numpy as np
 from matplotlib import pyplot as plt
