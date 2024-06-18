fc339631b2dd6c0b08295e0555713dea59f3e3eb
Lingjie Mei <lm5483@princeton.edu>
2024-06-17 17:24:23 -0700

    Add 12 lines to infinigen/assets/materials/text_no_barcode.py. Contributed as part of Infinigen-Indoors by Lingjie Mei.

---
+++ infinigen/assets/materials/text_no_barcode.py
@@ -0,0 +1,12 @@
+# Copyright (c) Princeton University.
+# This source code is licensed under the BSD 3-Clause license found in the LICENSE file in the root directory of this source tree.
+
+# Authors: Lingjie Mei
+import numpy as np
+
+from .text import Text
+from .text import make_sphere
+
+
+def apply(obj, selection=None, bbox=(0, 1, 0, 1), emission=0, **kwargs):
+    Text(np.random.randint(1e5), False, emission).apply(obj, selection, bbox, **kwargs)
