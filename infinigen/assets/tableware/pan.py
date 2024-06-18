dc13ce52eafc162589a2b04fc1cf976a435277fa
Alexander Raistrick <araistrick@princeton.edu>
2024-06-17 17:24:21 -0700

    Add 3 lines to infinigen/assets/tableware/pan.py. Contributed as part of Infinigen-Indoors by Alexander Raistrick.

--- infinigen/assets/tableware/pan.py
+++ infinigen/assets/tableware/pan.py
@@ -2,6 +2,9 @@
 # This source code is licensed under the BSD 3-Clause license found in the LICENSE file in the root directory
 # of this source tree.
 
+# Authors: 
+# - Lingjie Mei
+# - Karhan Kayan: fix cutter bug
 
 import bpy
 import bmesh
