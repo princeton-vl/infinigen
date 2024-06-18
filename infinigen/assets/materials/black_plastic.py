d491e102fa96cfdde9469fe608b7b0be17dc126b
Meenal Parakh <meenalp@princeton.edu>
2024-06-17 17:24:24 -0700

    Add 17 lines to infinigen/assets/materials/black_plastic.py. Contributed as part of Infinigen-Indoors by Meenal Parakh.

---
+++ infinigen/assets/materials/black_plastic.py
@@ -0,0 +1,17 @@
+from numpy.random import uniform as U
+from infinigen.core.nodes.node_wrangler import Nodes, NodeWrangler
+from infinigen.core.util.color import hsv2rgba
+
+# used in ceiling lights and tv
+
+def shader_black(nw: NodeWrangler):
+    # Code generated using version 2.6.5 of the node_transpiler
+
+    color = hsv2rgba(
+        U(0.45, 0.55),
+        U(0, 0.1),
+        U(0, 1)
+    )
+    principled_bsdf = nw.new_node(Nodes.PrincipledBSDF, input_kwargs={'Base Color': color})
+    
+    material_output = nw.new_node(Nodes.MaterialOutput, input_kwargs={'Surface': principled_bsdf}, attrs={'is_active_output': True})
