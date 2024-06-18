95ddc22b969fc5a1014c7b4e8a73894154322f3a
Lingjie Mei <lm5483@princeton.edu>
2024-06-17 17:24:26 -0700

    Add 12 lines to infinigen/assets/materials/woods/staggered_wood_tile.py. Contributed as part of Infinigen-Indoors by Lingjie Mei.

---
+++ infinigen/assets/materials/woods/staggered_wood_tile.py
@@ -0,0 +1,12 @@
+# Copyright (c) Princeton University.
+# This source code is licensed under the BSD 3-Clause license found in the LICENSE file in the root directory of this source tree.
+
+# Authors: Lingjie Mei
+from .. import tile
+from .wood import shader_wood
+from ...utils.object import new_plane
+
+
+def apply(obj, selection=None, vertical=False, scale=None, alternating=None, shape=None, **kwargs):
+    shader_func = shader_wood
+    tile.apply(obj, selection, vertical, shader_func, scale, alternating, 'staggered', **kwargs)
