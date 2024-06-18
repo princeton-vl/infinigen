2ff07440243d0dee94036800fefa596095185a7b
Alexander Raistrick <araistrick@princeton.edu>
2024-06-17 17:24:19 -0700

    Add 42 lines to infinigen/core/constraints/constraint_language/types.py. Contributed as part of Infinigen-Indoors by Alexander Raistrick.

---
+++ infinigen/core/constraints/constraint_language/types.py
@@ -0,0 +1,42 @@
+# Copyright (c) Princeton University.
+# This source code is licensed under the BSD 3-Clause license found in the LICENSE file in the root directory
+# of this source tree.
+
+# Authors: Alexander Raistrick
+
+from enum import Enum
+from typing import Any
+from dataclasses import dataclass
+import functools
+
+nodedataclass_kwargs = dict(eq=False, order=False)
+
+def _nodeclass_bool_throw(self):
+    raise RuntimeError(
+        f'Attempted to convert {self.__class__} to bool, '
+        f"truth value of {self} is ambiguous. Constraint language must use  * instead of `and`, etc since python bool ops are not overridable"
+    )
+
+def nodedataclass(frozen=False):
+    def decorator(cls):
+        ddec = dataclass(eq=False, order=False, frozen=frozen)
+        cls = ddec(cls)
+        cls.__bool__ = _nodeclass_bool_throw
+        return cls
+    return decorator
+
+@nodedataclass()
+class Node:
+
+    def children(self):
+        for k, v in self.__dict__.items():
+            if isinstance(v, Node):
+                yield k, v
+
+    def traverse(self, inorder=True):
+        if inorder:
+            yield self
+        for _, c in self.children():
+            yield from c.traverse(inorder=inorder)
+        if not inorder:
+            yield self
