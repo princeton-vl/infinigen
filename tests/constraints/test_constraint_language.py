dd5273645c5ad81cf676bb319b95e50042498952
Alexander Raistrick <araistrick@princeton.edu>
2024-06-17 17:24:11 -0700

    Add 43 lines to tests/constraints/test_constraint_language.py. Contributed as part of Infinigen-Indoors by Alexander Raistrick.

---
+++ tests/constraints/test_constraint_language.py
@@ -0,0 +1,43 @@
+# Copyright (c) Princeton University.
+# This source code is licensed under the BSD 3-Clause license found in the LICENSE file in the root directory
+# of this source tree.
+
+# Authors: Alexander Raistrick
+
+from infinigen_examples import indoor_constraint_examples as ex
+from infinigen.core.constraints import (
+    constraint_language as cl,
+    reasoning as r
+)
+
+def test_residential():
+
+    cons = ex.home_constraints()
+
+    assert isinstance(cons, cl.Node)
+    assert isinstance(repr(cons), str)
+    
+def test_operators_simple():
+
+    val = cl.constant(value=1)
+    assert hasattr(val, '__add__')
+
+    comp = cl.constant(1) + cl.constant(2)
+    assert isinstance(comp, cl.Expression)
+    val = comp()
+    assert val == 3, val
+
+    comp = cl.constant(1) < cl.constant(2)
+    assert isinstance(comp, cl.Expression)
+    assert comp() is True
+
+def test_operators_cast():
+
+    comp = cl.constant(1) + 2
+    assert isinstance(comp, cl.ScalarOperatorExpression)
+    assert comp() == 3
+
+def test_associative_construction():
+
+    comp = cl.constant(1) + cl.constant(2) + cl.constant(3)
+    assert len(list(comp.traverse())) == 4 # 1 for additions, 3 for constants
