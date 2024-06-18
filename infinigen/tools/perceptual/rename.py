2a7e3c06063e2b79edcb20b0a814d7973b4208ba
Karhan Kaan Kayan <karhan@princeton.edu>
2024-06-17 17:24:13 -0700

    Add 13 lines to infinigen/tools/perceptual/rename.py. Contributed as part of Infinigen-Indoors by Karhan Kaan Kayan.

---
+++ infinigen/tools/perceptual/rename.py
@@ -0,0 +1,13 @@
+# Copyright (c) Princeton University.
+# This source code is licensed under the BSD 3-Clause license found in the LICENSE file in the root directory of this source tree.
+
+# Authors: Karhan Kayan
+
+import os
+import sys
+
+
+if __name__ == '__main__':
+
+
+
