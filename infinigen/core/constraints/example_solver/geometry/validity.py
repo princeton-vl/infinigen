a475584445c6258c4adc2b2bf4119861778654bb
Karhan Kaan Kayan <karhan@princeton.edu>
2024-06-17 17:24:18 -0700

    Add 55 lines to infinigen/core/constraints/example_solver/geometry/validity.py. Contributed as part of Infinigen-Indoors by Karhan Kaan Kayan.

--- infinigen/core/constraints/example_solver/geometry/validity.py
+++ infinigen/core/constraints/example_solver/geometry/validity.py
@@ -1,17 +1,65 @@
+# Copyright (c) Princeton University.
+# This source code is licensed under the BSD 3-Clause license found in the LICENSE file in the root directory of this source tree.
+
+# Authors: Karhan Kayan
+
 import logging
 
+import bpy 
 from shapely.geometry import Point, Polygon, MultiPolygon
 
 from infinigen.core.util import blender as butil
 import infinigen.core.constraints.constraint_language as cl
+from infinigen.core.constraints.constraint_language.util import meshes_from_names, blender_objs_from_names, subset, project_to_xy_poly
 from infinigen.core.constraints.example_solver.state_def import State, ObjectState, RelationState
 from infinigen.core.constraints.evaluator.node_impl.trimesh_geometry import constrain_contact, any_touching
 from infinigen.core.constraints.example_solver.geometry.stability import stable_against, supported_by
+
 from infinigen.core import tags as t
 
 import gin
 
 logger = logging.getLogger(__name__)
+
+def check_pre_move_validity(scene, a, parent_dict, dx, dy):
+    """
+    """
+    parent = parent_dict[a]
+    a_mesh = meshes_from_names(scene, a)[0]
+    parent_mesh = meshes_from_names(scene, parent)[0]
+    blender_mesh = blender_objs_from_names(a)[0]
+    
+
+    # move a mesh by dx, dy and check if the projection of a_mesh is contained in parent_mesh
+    # a_mesh.apply_transform(trimesh.transformations.compose_matrix(translate=[dx,dy,0]))
+    a_poly = project_to_xy_poly(a_mesh)
+    parent_poly = project_to_xy_poly(parent_mesh)
+    centroid = a_poly.centroid
+    new_centroid = Point([centroid.x + dx, centroid.y + dy])
+    # plot 
+    # fig, ax = plt.subplots()
+    # if isinstance(parent_poly, Polygon):
+    #     x, y = parent_poly.exterior.xy
+    #     ax.fill(x, y, alpha=0.5, fc='red', ec='black', label='Polygon b')
+    # elif isinstance(parent_poly, MultiPolygon):
+    #     for sub_poly in parent_poly.geoms:
+    #         x, y = sub_poly.exterior.xy
+    #         ax.fill(x, y, alpha=0.5, fc='red', ec='black', label='Polygon b')
+    # ax.plot(centroid.x, centroid.y, 'o', color='black', label='Random point')
+    # plt.show()
+    # scene.show()
+
+    if isinstance(a_poly, Polygon):
+        if not parent_poly.contains(new_centroid):
+            # print("not contained")
+            return False
+    elif isinstance(a_poly, MultiPolygon):
+        for sub_poly in a_poly.geoms:
+            if not parent_poly.contains(new_centroid):
+                # print("not contained")
+                return False
+
+    return True
 
 def all_relations_valid(state, name):
 
@@ -36,6 +84,7 @@ def check_post_move_validity(
     visualize=False
 ): 
 
+    scene = state.trimesh_scene
     objstate = state.objs[name]
 
     collision_objs = [
@@ -44,8 +93,10 @@ def check_post_move_validity(
     ]
 
     if len(collision_objs) == 0: 
+        return True
     
     if not all_relations_valid(state, name):
+
         if visualize:
             vis_obj = butil.copy(objstate.obj)
             vis_obj.name = f'validity_relations_fail_{name}'
@@ -65,3 +116,7 @@ def check_post_move_validity(
 
         contact_names = [[x for x in t.names if not x.startswith('_')] for t in touch.contacts]
         logger.debug(f'validity failed - {name} touched {contact_names[0]} {len(contact_names)=}')
+        return False
+    
+    # supposed to go through the consgraph here
+    return True
