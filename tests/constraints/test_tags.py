d6ec51182ae7c27754759171966eaa833e426f72
Alexander Raistrick <araistrick@princeton.edu>
2024-06-17 17:24:11 -0700

    Add 15 lines to tests/constraints/test_tags.py. Contributed as part of Infinigen-Indoors by Alexander Raistrick.

---
+++ tests/constraints/test_tags.py
@@ -0,0 +1,15 @@
+# Copyright (c) Princeton University.
+# This source code is licensed under the BSD 3-Clause license found in the LICENSE file in the root directory
+# of this source tree.
+
+# Authors: Alexander Raistrick
+
+from infinigen.core import tags as t
+
+def test_implies():
+
+    assert t.implies(set(), set())
+    assert t.implies({t.Subpart.Wall}, {t.Subpart.Wall})
+    assert t.implies({t.Subpart.Wall}, set())
+
+    assert t.implies({t.Semantics.Room, t.Variable('room')}, {t.Semantics.Room})
