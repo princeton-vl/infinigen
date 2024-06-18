0d18024cdf0a317eaa390f6011a591309dbbfcd9
Lingjie Mei <lm5483@princeton.edu>
2024-06-17 17:24:27 -0700

    Add 110 lines to infinigen/assets/bathroom/hardware.py. Contributed as part of Infinigen-Indoors by Lingjie Mei.

---
+++ infinigen/assets/bathroom/hardware.py
@@ -0,0 +1,110 @@
+# Copyright (c) Princeton University.
+# This source code is licensed under the BSD 3-Clause license found in the LICENSE file in the root directory of this source tree.
+
+# Authors: Lingjie Mei
+import bpy
+import numpy as np
+from numpy.random import uniform
+
+from infinigen.assets.utils.decorate import subsurf
+from infinigen.assets.utils.object import join_objects, new_base_cylinder, new_cube
+from infinigen.core.placement.factory import AssetFactory
+from infinigen.core.util.math import FixedSeed
+from infinigen.core.util import blender as butil
+from infinigen.core.util.random import log_uniform
+
+
+class HardwareFactory(AssetFactory):
+    def __init__(self, factory_seed, coarse=False):
+        super(HardwareFactory, self).__init__(factory_seed, coarse)
+        with FixedSeed(self.factory_seed):
+            self.attachment_radius = uniform(.02, .03)
+            self.attachment_depth = uniform(.01, .015)
+            self.radius = uniform(.01, .015)
+            self.depth = uniform(.06, .1)
+            self.is_circular = uniform() < .5
+            self.hardware_type = np.random.choice(['hook', 'holder', 'bar', 'ring'])
+            self.hook_length = self.attachment_radius * uniform(2, 4)
+            self.holder_length = uniform(.15, .25)
+            self.bar_length = uniform(.4, .8)
+            self.extension_length = self.attachment_radius * uniform(2, 3)
+            self.ring_radius = log_uniform(2, 6) * self.attachment_radius
+
+    def make_attachment(self):
+        base = new_base_cylinder() if self.is_circular else new_cube()
+        base.scale = self.attachment_radius, self.attachment_radius, self.attachment_depth / 2
+        base.rotation_euler[0] = np.pi / 2
+        base.location[1] = -self.attachment_depth / 2
+        butil.apply_transform(base, True)
+
+        rod = new_base_cylinder() if self.is_circular else new_cube()
+        rod.scale = self.radius, self.radius, self.depth / 2
+        rod.rotation_euler[0] = np.pi / 2
+        rod.location[1] = -self.depth / 2
+        butil.apply_transform(rod, True)
+        obj = join_objects([base, rod])
+        return obj
+
+    def make_hook(self):
+        obj = new_base_cylinder() if self.is_circular else new_cube()
+        obj.scale = self.radius, self.radius, self.hook_length / 2
+        butil.apply_transform(obj)
+        return obj
+
+    def make_holder(self):
+        obj = new_base_cylinder() if self.is_circular else new_cube()
+        obj.scale = self.radius, self.radius, (self.holder_length + self.extension_length) / 2
+        obj.rotation_euler[1] = np.pi / 2
+        obj.location[0] = (self.holder_length - self.extension_length) / 2
+        butil.apply_transform(obj, True)
+        return obj
+
+    def make_bar(self):
+        obj = new_base_cylinder() if self.is_circular else new_cube()
+        obj.scale = self.radius, self.radius, self.bar_length / 2 + self.extension_length
+        obj.rotation_euler[1] = np.pi / 2
+        obj.location[0] = self.bar_length / 2
+        butil.apply_transform(obj, True)
+        return obj
+
+    def make_ring(self):
+        bpy.ops.mesh.primitive_torus_add(
+            major_segments=128, major_radius=self.ring_radius,
+            minor_radius=self.radius * uniform(.4, .7)
+        )
+        obj = bpy.context.active_object
+        obj.rotation_euler[0] = np.pi / 2
+        obj.location = 0, self.attachment_depth, -self.ring_radius
+        butil.apply_transform(obj, True)
+        subsurf(obj, 2)
+        return obj
+
+    def create_asset(self, **params) -> bpy.types.Object:
+        match self.hardware_type:
+            case 'hook':
+                extra = self.make_hook()
+            case 'holder':
+                extra = self.make_holder()
+            case 'bar':
+                extra = self.make_bar()
+            case 'ring':
+                extra = self.make_ring()
+            case _:
+                return self.make_attachment()
+        extra.scale = [1 + 1e-3] * 3
+        extra.location[1] = -self.depth
+        butil.apply_transform(extra, True)
+        parts = [self.make_attachment(), extra]
+        if self.hardware_type == 'bar':
+            attachment_ = self.make_attachment()
+            attachment_.location[0] = self.bar_length
+            butil.apply_transform(attachment_, True)
+            parts.append(attachment_)
+        obj = join_objects(parts)
+        obj.rotation_euler[-1] = np.pi / 2
+        butil.apply_transform(obj)
+        return obj
+
+    def finalize_assets(self, assets):
+        self.surface.apply(assets, metal_color='plain')
+            self.edge_wear.apply(assets)
