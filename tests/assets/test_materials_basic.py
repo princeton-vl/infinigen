07fcd9eb97e243c019123b3fab9886fcf15ee85e
Lingjie Mei <lm5483@princeton.edu>
2024-06-17 17:24:12 -0700

    Add 7 lines to tests/assets/test_materials_basic.py. Contributed as part of Infinigen-Indoors by Lingjie Mei.

--- tests/assets/test_materials_basic.py
+++ tests/assets/test_materials_basic.py
@@ -15,12 +15,15 @@ from infinigen.core.util import blender as butil
 
 from infinigen_examples.util.test_utils import (setup_gin, load_txt_list, import_item)
 
+
 def check_material_runs(pathspec):
     butil.clear_scene()
     bpy.ops.mesh.primitive_ico_sphere_add(radius=.8, subdivisions=5)
     asset = bpy.context.active_object
 
     mat = import_item(pathspec)
+    if type(mat) is type:
+        mat = mat(0)
     mat.apply(asset)
 
     # should not crash for input LIST of objects
@@ -30,11 +33,15 @@ def check_material_runs(pathspec):
 
 
 
+
 @pytest.mark.nature
 @pytest.mark.parametrize('pathspec', load_txt_list('tests/assets/list_nature_materials.txt'))
 def test_nature_material_runs(pathspec, **kwargs):
     setup_gin('infinigen_examples/configs_nature')
     check_material_runs(pathspec)
+
+
 @pytest.mark.parametrize('pathspec', load_txt_list('tests/assets/list_indoor_materials.txt'))
 def test_indoor_material_runs(pathspec, **kwargs):
     setup_gin('infinigen_examples/configs_indoor')
+    check_material_runs(pathspec)
