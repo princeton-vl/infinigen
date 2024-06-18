8a766fdd41d2853ca918c90fd8f1e8e11c95fe2f
Alexander Raistrick <araistrick@princeton.edu>
2024-06-17 17:24:19 -0700

    Add 32 lines to infinigen/core/constraints/constraint_language/result.py. Contributed as part of Infinigen-Indoors by Alexander Raistrick.

---
+++ infinigen/core/constraints/constraint_language/result.py
@@ -0,0 +1,32 @@
+# Copyright (c) Princeton University.
+# This source code is licensed under the BSD 3-Clause license found in the LICENSE file in the root directory
+# of this source tree.
+
+# Authors: Alexander Raistrick
+
+import typing
+from dataclasses import dataclass, field
+
+import numpy as np
+
+from .types import Node
+from .expression import BoolExpression, ScalarExpression, nodedataclass
+
+@nodedataclass()
+class Problem(Node):
+
+    constraints: dict[str, BoolExpression]
+    score_terms: dict[str, ScalarExpression]    
+
+    def __post_init__(self):
+
+        if isinstance(self.constraints, list):
+            self.constraints = {i: c for i, c in enumerate(self.constraints)}
+        if isinstance(self.score_terms, list):
+            self.score_terms = {i: s for i, s in enumerate(self.score_terms)}
+
+    def children(self):
+        for i, v in enumerate(self.constraints.values()):
+            yield f'constraints[{i}]', v
+        for i, v in enumerate(self.score_terms.values()):
+            yield f'score_terms[{i}]', v
