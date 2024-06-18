cf57f6f861333f7a98d9aeee1102a0f4e5df67b7
Alexander Raistrick <araistrick@princeton.edu>
2024-06-17 17:24:33 -0700

    Add 1 lines to infinigen/assets/lighting/three_point_lighting.py. Contributed as part of Infinigen-Indoors by Alexander Raistrick.

--- infinigen/assets/lighting/three_point_lighting.py
+++ infinigen/assets/lighting/three_point_lighting.py
@@ -2,6 +2,7 @@
 # This source code is licensed under the BSD 3-Clause license found in the LICENSE file in the root directory of this source tree.
 
 # Authors: Lingjie Mei
+
 import bpy
 import numpy as np
 from numpy.random import uniform
