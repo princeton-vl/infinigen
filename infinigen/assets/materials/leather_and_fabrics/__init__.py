074197412f87b0794ea702aad079e6d392ab1232
Lingjie Mei <lm5483@princeton.edu>
2024-06-17 17:24:25 -0700

    Add 20 lines to infinigen/assets/materials/leather_and_fabrics/__init__.py. Contributed as part of Infinigen-Indoors by Lingjie Mei.

---
+++ infinigen/assets/materials/leather_and_fabrics/__init__.py
@@ -0,0 +1,20 @@
+# Copyright (c) Princeton University.
+# This source code is licensed under the BSD 3-Clause license found in the LICENSE file in the root directory of this source tree.
+
+# Authors: Lingjie Mei
+from .general_fabric import shader_fabric
+from .lined_fabric import shader_lined_fur_base
+from .coarse_knit_fabric import shader_fabric_random as shader_coarse_fabric_random
+from .fine_knit_fabric import shader_fabric_random as shader_fine_fabric_random
+from .leather import shader_leather
+from .sofa_fabric import shader_sofa_fabric
+
+from infinigen.core.util.random import random_general as rg
+from .. import common
+from ...utils.uv import unwrap_faces
+
+
+
+def apply(obj, selection=None, **kwargs):
+    unwrap_faces(obj, selection)
+    common.apply(obj, rg(fabric_shader_list), selection=selection, **kwargs)
