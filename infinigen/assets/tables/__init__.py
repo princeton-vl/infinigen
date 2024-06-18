b40d250951f618fafab68002e0882e3e96a89e13
Lingjie Mei <lm5483@princeton.edu>
2024-06-17 17:24:21 -0700

    Add 6 lines to infinigen/assets/tables/__init__.py. Contributed as part of Infinigen-Indoors by Lingjie Mei.

---
+++ infinigen/assets/tables/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) Princeton University.
+# This source code is licensed under the BSD 3-Clause license found in the LICENSE file in the root directory of this source tree.
+
+# Authors: Lingjie Mei
+from .cocktail_table import TableCocktailFactory
+from .table_top import TableTopFactory
