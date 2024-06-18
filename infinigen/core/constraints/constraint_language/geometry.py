243c6dd8457b47c8c3b6efee9e7881181b71a295
Karhan Kaan Kayan <karhan@princeton.edu>
2024-06-17 17:24:19 -0700

    Add 35 lines to infinigen/core/constraints/constraint_language/geometry.py. Contributed as part of Infinigen-Indoors by Karhan Kaan Kayan.

--- infinigen/core/constraints/constraint_language/geometry.py
+++ infinigen/core/constraints/constraint_language/geometry.py
@@ -1,3 +1,8 @@
+# Copyright (c) Princeton University.
+# This source code is licensed under the BSD 3-Clause license found in the LICENSE file in the root directory of this source tree.
+
+# Authors: Karhan Kayan
+
 import typing
 from dataclasses import dataclass, field
 
@@ -5,9 +10,18 @@ import numpy as np
 
 from .expression import Expression, BoolExpression, ScalarExpression, nodedataclass
 from .set_reasoning import ObjectSetExpression
+
+@nodedataclass()
 class center_stable_surface_dist(ScalarExpression):
+    objs: ObjectSetExpression
+
+@nodedataclass()
+class accessibility_cost(ScalarExpression):
+    objs: ObjectSetExpression
+    others: ObjectSetExpression
     normal: np.array = field(default=np.array([1, 0, 0]))
     dist: float = 1.0
+
     def __post_init__(self):
         if isinstance(self.normal, (list, tuple)):
             self.normal = np.array(self.normal)
@@ -30,26 +44,47 @@ class min_distance_internal(ScalarExpression):
     objs: ObjectSetExpression
     
 @nodedataclass()
+class focus_score(ScalarExpression):
     objs: ObjectSetExpression
     others: ObjectSetExpression
+
 @nodedataclass()
+class angle_alignment_cost(ScalarExpression):
     objs: ObjectSetExpression
     others: ObjectSetExpression
+    others_tags: set = None
+
     def __post_init__(self):
         if self.others_tags is None:
             self.others_tags = set()
         assert isinstance(self.others_tags, set), type(self.others_tags)
 
 @nodedataclass()
+class freespace_2d(ScalarExpression):
     objs: ObjectSetExpression
     others: ObjectSetExpression
+
 @nodedataclass()
+class min_dist_2d(ScalarExpression):
     objs: ObjectSetExpression
     others: ObjectSetExpression
+
 @nodedataclass()
+class rotational_asymmetry(ScalarExpression):
     objs: ObjectSetExpression
+
+@nodedataclass()
+class reflectional_asymmetry(ScalarExpression):
+    objs: ObjectSetExpression
+    others: ObjectSetExpression
+    use_long_plane: bool = True
 
 @ObjectSetExpression.register_postfix_func
 @nodedataclass()
 class volume(ScalarExpression):
+    objs: ObjectSetExpression
+    dims: int | tuple = 3
+
+@nodedataclass()
+class coplanarity_cost(ScalarExpression):
     objs: ObjectSetExpression
