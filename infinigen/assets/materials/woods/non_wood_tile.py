e8728a7544edac98c692fe9e32772dfb02790a27
Lingjie Mei <lm5483@princeton.edu>
2024-06-17 17:24:26 -0700

    Add 22 lines to infinigen/assets/materials/woods/non_wood_tile.py. Contributed as part of Infinigen-Indoors by Lingjie Mei.

---
+++ infinigen/assets/materials/woods/non_wood_tile.py
@@ -0,0 +1,22 @@
+# Copyright (c) Princeton University.
+# This source code is licensed under the BSD 3-Clause license found in the LICENSE file in the root directory of this source tree.
+
+# Authors: Lingjie Mei
+import numpy as np
+
+
+def apply(
+    obj, selection=None, vertical=False, shader_func=None, scale=None, alternating=None, shape=None,
+    **kwargs
+):
+    from .. import tile
+    from .wood import shader_wood
+    shader_funcs = tile.get_shader_funcs()
+    shader_funcs = [(f, w) for f, w in shader_funcs if f != shader_wood]
+    funcs, weights = zip(*shader_funcs)
+    weights = np.array(weights) / sum(weights)
+    if shader_func is None:
+        shader_func = np.random.choice(funcs, p=weights)
+    if shape is None:
+        shape = np.random.choice(['square', 'hexagon', 'rectangle'])
+    tile.apply(obj, selection, vertical, shader_func, scale, alternating, shape, **kwargs)
