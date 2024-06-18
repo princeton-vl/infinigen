54b1e29e9d6c95fc112be48b73bd7e09e3f26eed
Alexander Raistrick <araistrick@princeton.edu>
2024-06-17 17:24:17 -0700

    Add 3 lines to infinigen/core/constraints/example_solver/room/utils.py. Contributed as part of Infinigen-Indoors by Alexander Raistrick.

--- infinigen/core/constraints/example_solver/room/utils.py
+++ infinigen/core/constraints/example_solver/room/utils.py
@@ -2,6 +2,9 @@
 # This source code is licensed under the BSD 3-Clause license found in the LICENSE file in the root directory
 # of this source tree.
 
+# Authors: 
+# - Lingjie Mei: primary author
+# - Karhan Kayan: fix constants
 
 from collections import defaultdict
 
