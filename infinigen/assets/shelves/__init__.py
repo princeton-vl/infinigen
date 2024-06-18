e98f5ba071acf90d6384ec663887c82b44284b02
Beining Han <bh7023@princeton.edu>
2024-06-17 17:24:28 -0700

    Add 8 lines to infinigen/assets/shelves/__init__.py. Contributed as part of Infinigen-Indoors by Beining Han.

---
+++ infinigen/assets/shelves/__init__.py
@@ -0,0 +1,8 @@
+from .simple_bookcase import SimpleBookcaseFactory
+from .cell_shelf import CellShelfFactory, TVStandFactory
+from .triangle_shelf import TriangleShelfFactory
+from .large_shelf import LargeShelfFactory
+from .doors import CabinetDoorBaseFactory
+from .single_cabinet import SingleCabinetFactory
+from .kitchen_cabinet import KitchenCabinetFactory
+from .kitchen_space import KitchenSpaceFactory, KitchenIslandFactory
