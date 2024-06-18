d8f90636884d9f6091082add786379ff077d22eb
Alexander Raistrick <araistrick@princeton.edu>
2024-06-17 17:24:26 -0700

    Add 2 lines to infinigen/assets/materials/woods/wood_old.py. Contributed as part of Infinigen-Indoors by Alexander Raistrick.

--- infinigen/assets/materials/woods/wood_old.py
+++ infinigen/assets/materials/woods/wood_old.py
@@ -1,6 +1,8 @@
 # Copyright (c) Princeton University.
+# This source code is licensed under the BSD 3-Clause license found in the LICENSE file in the root directory of this source tree.
 
 # Authors: Mingzhe Wang, Lingjie Mei
+
 import math as ma
 
 import numpy as np
