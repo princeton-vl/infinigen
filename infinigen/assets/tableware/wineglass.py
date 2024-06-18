6a714869c17a1fee7efc5a7ec86a52cbc2df5fad
Lingjie Mei <lm5483@princeton.edu>
2024-06-17 17:24:21 -0700

    Add 46 lines to infinigen/assets/tableware/wineglass.py. Contributed as part of Infinigen-Indoors by Lingjie Mei.

---
+++ infinigen/assets/tableware/wineglass.py
@@ -0,0 +1,46 @@
+# Copyright (c) Princeton University.
+# This source code is licensed under the BSD 3-Clause license found in the LICENSE file in the root directory of this source tree.
+
+# Authors: Lingjie Mei
+import bpy
+import numpy as np
+from numpy.random import uniform
+
+from infinigen.assets.tableware.base import TablewareFactory
+from infinigen.assets.materials import glass
+from infinigen.assets.utils.decorate import subsurf
+from infinigen.assets.utils.draw import spin
+from infinigen.core.util.random import log_uniform
+from infinigen.core.util.math import FixedSeed
+from infinigen.core.util import blender as butil
+
+
+class WineglassFactory(TablewareFactory):
+
+    def __init__(self, factory_seed, coarse=False):
+        super().__init__(factory_seed, coarse)
+        with FixedSeed(factory_seed):
+            self.x_end = .25
+            self.z_length = log_uniform(.6, 2.)
+            self.z_cup = uniform(.3, .6) * self.z_length
+            self.z_mid = self.z_cup + uniform(.3, .5) * (self.z_length - self.z_cup)
+            self.x_neck = log_uniform(.01, .02)
+            self.x_top = self.x_end * log_uniform(1, 1.4)
+            self.x_mid = self.x_top * log_uniform(.9, 1.2)
+            self.has_guard = False
+            self.thickness = uniform(.01, .03)
+            self.surface = glass
+            self.scale = log_uniform(.1, .3)
+
+    def create_asset(self, **params) -> bpy.types.Object:
+        z_bottom = self.z_length * log_uniform(.01, .05)
+        x_anchors = self.x_end, self.x_end / 2, self.x_neck, self.x_neck, self.x_mid, self.x_top
+        z_anchors = 0, z_bottom / 2, z_bottom, self.z_cup, self.z_mid, self.z_length
+        anchors = x_anchors, np.zeros_like(x_anchors), z_anchors
+        obj = spin(anchors, [0, 1, 2, 3], 4, 16)
+        subsurf(obj, 2)
+        butil.modify_mesh(obj, 'SOLIDIFY', thickness=self.thickness)
+        subsurf(obj, 1)
+        obj.scale = [self.scale] * 3
+        butil.apply_transform(obj)
+        return obj
