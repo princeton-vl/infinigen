00fd65645cc16b4dc6ee19ceedbb5b958c1fa8d7
Alexander Raistrick <araistrick@princeton.edu>
2024-06-17 17:24:23 -0700

    Add 2 lines to infinigen/assets/materials/plastic.py. Contributed as part of Infinigen-Indoors by Alexander Raistrick.

--- infinigen/assets/materials/plastic.py
+++ infinigen/assets/materials/plastic.py
@@ -1,7 +1,9 @@
 # Copyright (c) Princeton University.
 # This source code is licensed under the GPL license found in the LICENSE file in the root directory of this
 # source tree.
+
 # Authors: Mingzhe Wang, Lingjie Mei
+
 import colorsys
 
 from infinigen.assets.materials.plastics.plastic_rough import shader_rough_plastic
