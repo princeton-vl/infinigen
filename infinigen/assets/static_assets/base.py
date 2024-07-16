62f4430628d44060f82fbe15b0a0d62f65b231e5
karhankayan <karhan@princeton.edu>
2024-07-16 15:04:32 -0400

    add copyright notice

--- infinigen/assets/static_assets/base.py
+++ infinigen/assets/static_assets/base.py
@@ -1,3 +1,9 @@
+# Copyright (c) Princeton University.
+# This source code is licensed under the BSD 3-Clause license found in the LICENSE file in the root directory of this source tree.
+
+# Authors:
+# - Karhan Kayan
+
 import os
 from typing import Callable, Dict, Optional
 
