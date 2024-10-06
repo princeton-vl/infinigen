05b48a9175e6a9ffc85c1d6a01f8218a936a0a84
Petr Leontev <raianknight@gmail.com>
2024-10-06 22:36:40 +0100

    Delete infinigen/datagen/util/is_wsl.py

--- infinigen/datagen/util/is_wsl.py
+++
@@ -1,8 +0,0 @@
-import platform
-
-def is_wsl(v: str = platform.uname().release) -> int:
-    # WSL v1 and v2
-    if v.endswith("-Microsoft") or v.endswith("microsoft-standard-WSL2"):
-        return True
-
-    return False
