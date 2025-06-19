38446d54f0391e66620118a4fa7f11e1ca691d0a
Abhishek Joshi <aj9794@princeton.edu>
2025-06-19 14:26:12 -0400

    Add 44 lines for Infinigen-Sim, last edited by Abhishek Joshi in scripts/spawn_asset.py
    Commit made automatically to show authorship. This commit is directly not usable.

---
+++ scripts/spawn_asset.py
@@ -0,0 +1,44 @@
+import argparse
+import pprint
+from pathlib import Path
+
+from infinigen.core.sim import sim_factory as sf
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-n", "--name", type=str, required=True, help="name of the asset"
+    )
+    parser.add_argument("-s", "--seed", type=int, default=0)
+    parser.add_argument(
+        "-exp",
+        "--exporter",
+        type=str,
+        default="mjcf",
+        help="exporter to use to spawn asset",
+    )
+    parser.add_argument(
+        "-dir",
+        "--export_dir",
+        type=str,
+        default="./sim_exports",
+        help="directory to export asset to",
+    )
+    parser.add_argument(
+        "-c",
+        "--include_collisions",
+        action="store_true",
+        help="directory to export asset to",
+    )
+
+    args = parser.parse_args()
+    export_path, semantic_mapping = sf.spawn_simready(
+        name=args.name,
+        seed=args.seed,
+        exporter=args.exporter,
+        export_dir=Path(args.export_dir),
+        visual_only=not args.include_collisions,
+    )
+    pprint.pprint(semantic_mapping)
+
+    print(f"Exported to {export_path.resolve()}")
