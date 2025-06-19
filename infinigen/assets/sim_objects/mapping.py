74b73c3993e488a2dbcb4fd13a0712b59305a7c4
Hongyu Wen <hongyu.wen@princeton.edu>
2025-06-19 14:26:16 -0400

    Add 6 lines for Infinigen-Sim, last edited by Hongyu Wen in infinigen/assets/sim_objects/mapping.py
    Commit made automatically to show authorship. This commit is directly not usable.

--- infinigen/assets/sim_objects/mapping.py
+++ infinigen/assets/sim_objects/mapping.py
@@ -4,6 +4,8 @@ from infinigen.assets.sim_objects.dishwasher import DishwasherFactory
 from infinigen.assets.sim_objects.doublefridge import DoublefridgeFactory
 from infinigen.assets.sim_objects.lamp import LampFactory
 from infinigen.assets.sim_objects.multidoublefridge import MultiDoublefridgeFactory
+from infinigen.assets.sim_objects.multifridge import MultifridgeFactory
+from infinigen.assets.sim_objects.singlefridge import SinglefridgeFactory
 
 # add newly transpiled assets here
 
@@ -11,6 +13,10 @@ OBJECT_CLASS_MAP = {
     "door": DoorFactory,
     "toaster": ToasterFactory,
     "dishwasher": DishwasherFactory,
+    "multifridge": MultifridgeFactory,
+    "singlefridge": SinglefridgeFactory,
+    "doublefridge": DoublefridgeFactory,
+    "multidoublefridge": MultiDoublefridgeFactory,
     "lamp": LampFactory,
     # add newly transpiled assets here
 }
