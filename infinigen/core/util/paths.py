630374722b2e5bc21e9798c2ce24e9411c051d3d
Abhishek Joshi <aj9794@princeton.edu>
2025-06-19 14:26:12 -0400

    Add 25 lines for Infinigen-Sim, last edited by Abhishek Joshi in infinigen/core/util/paths.py
    Commit made automatically to show authorship. This commit is directly not usable.

---
+++ infinigen/core/util/paths.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+import infinigen.assets.sim_blueprints as blueprints
+
+
+def blueprint_path_completion(blueprint_path, root=None):
+    """
+    Takes in a local xml path and returns a full path.
+        if @xml_path is absolute, do nothing
+        if @xml_path is not absolute, load xml that is shipped by the package
+
+    Args:
+        xml_path (str): local xml path
+        root (str): root folder for xml path. If not specified defaults to
+            infinigen.assets.sim_blueprints
+    Returns:
+        str: Full (absolute) xml path
+    """
+    if blueprint_path.startswith("/"):
+        full_path = blueprint_path
+    else:
+        if root is None:
+            root = blueprints
+        full_path = Path(root.__path__[0]) / blueprint_path
+    return Path(full_path) if isinstance(full_path, str) else full_path
